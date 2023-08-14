6040e00aff8f094b356d23f216872560dec779eb
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-14 11:08:29 +0300

    refactor ThirdTableFuelInfoSearchingService

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/ploughing/ThirdTableFuelInfoSearchingService.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/ploughing/ThirdTableFuelInfoSearchingService.java
@@ -13,13 +13,10 @@ import static by.aurorasoft.fuelinfosearcher.util.FuelDocumentRowFilterUtil.find
 @Service
 public final class ThirdTableFuelInfoSearchingService extends AbstractPloughingFuelInfoSearchingService {
     private static final String TABLE_NAME = "ВСПАШКА МЕЛИОРИРУЕМЫХ ЗЕМЕЛЬ";
-    private static final String[] ROUTING_LENGTHS = new String[]{
-            "Менее 150", "150–200", "201–300", "301–400", "401–600", "601–1000", "Более 1000"
-    };
     private static final int FIRST_FUEL_INFO_OFFSET = 2;
 
     public ThirdTableFuelInfoSearchingService(final FuelDocument fuelDocument) {
-        super(fuelDocument, TABLE_NAME, ROUTING_LENGTHS, FIRST_FUEL_INFO_OFFSET);
+        super(fuelDocument, TABLE_NAME, FIRST_FUEL_INFO_OFFSET);
     }
 
     @Override
