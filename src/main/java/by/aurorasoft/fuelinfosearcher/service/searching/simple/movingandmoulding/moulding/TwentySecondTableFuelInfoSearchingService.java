677a89279bc892ff93949936a24468778291c3b6
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-14 11:03:29 +0300

    refactor TwentySecondTableFuelInfoSearchingService

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/movingandmoulding/moulding/TwentySecondTableFuelInfoSearchingService.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/movingandmoulding/moulding/TwentySecondTableFuelInfoSearchingService.java
@@ -6,13 +6,10 @@ import org.springframework.stereotype.Service;
 @Service
 public final class TwentySecondTableFuelInfoSearchingService extends AbstractMouldingTableFuelInfoSearchingService {
     private static final String TABLE_NAME = "ПРЕССОВАНИЕ ЛЕНТ ЛЬНА";
-    private static final String[] ROUTING_LENGTHS = new String[]{
-            "Менее 150", "151…200", "201…300", "301…400", "401…600", "601…1000", "Более 1000"
-    };
     private static final int FIRST_FUEL_INFO_OFFSET = 1;
 
     public TwentySecondTableFuelInfoSearchingService(final FuelDocument fuelDocument) {
-        super(fuelDocument, TABLE_NAME, ROUTING_LENGTHS, FIRST_FUEL_INFO_OFFSET);
+        super(fuelDocument, TABLE_NAME, FIRST_FUEL_INFO_OFFSET);
     }
 
 }
