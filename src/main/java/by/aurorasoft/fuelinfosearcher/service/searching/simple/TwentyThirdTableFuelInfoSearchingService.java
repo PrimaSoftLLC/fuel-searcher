a36555d9e23630b771772feeea50df375781eee3
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-14 11:23:38 +0300

    refactor TwentyThirdTableFuelInfoSearchingService

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/TwentyThirdTableFuelInfoSearchingService.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/TwentyThirdTableFuelInfoSearchingService.java
@@ -9,12 +9,12 @@ import java.util.List;
 import java.util.Optional;
 
 import static by.aurorasoft.fuelinfosearcher.util.FuelDocumentRowFilterUtil.*;
-import static by.aurorasoft.fuelinfosearcher.util.FuelDocumentRowFilterUtil.findRowByYield;
+import static by.aurorasoft.fuelinfosearcher.util.FuelInfoSpecificationUtil.extractRoutingLength;
 
 @Service
 public final class TwentyThirdTableFuelInfoSearchingService extends AbstractSimpleTableFuelInfoSearchingService {
     private static final String TABLE_NAME = "ПОДБОР ПРОВЯЛЕННЫХ ТРАВ ИЗ ВАЛКОВ С ИЗМЕЛЬЧЕНИЕМ И ПОДАЧЕЙ В ТРАНСПОРТНЫЕ СРЕДСТВА";
-    private static final String[] ROUTING_LENGTHS = new String[]{
+    private static final String[] FUEL_INFO_HEADERS = new String[]{
             "Менее 150", "151…200", "201…300", "301…400", "401…600", "601…1000", "Более 1000"
     };
     private static final int FIRST_FUEL_INFO_OFFSET = 0;
@@ -24,7 +24,7 @@ public final class TwentyThirdTableFuelInfoSearchingService extends AbstractSimp
     private static final int CELL_INDEX_YIELD = 2;
 
     public TwentyThirdTableFuelInfoSearchingService(final FuelDocument fuelDocument) {
-        super(fuelDocument, TABLE_NAME, ROUTING_LENGTHS, FIRST_FUEL_INFO_OFFSET);
+        super(fuelDocument, TABLE_NAME, FUEL_INFO_HEADERS, FIRST_FUEL_INFO_OFFSET);
     }
 
     @Override
@@ -33,6 +33,11 @@ public final class TwentyThirdTableFuelInfoSearchingService extends AbstractSimp
         return findRowsByMachinery(elementTableRows, specification, CELL_INDEX_MACHINERY)
                 .flatMap(rows -> findRowsByWorkingWidth(rows, specification, CELL_INDEX_WORKING_WIDTH))
                 .flatMap(rows -> findRowByYield(rows, specification, CELL_INDEX_YIELD));
+    }
+
+    @Override
+    protected String extractFuelInfoHeaderCellValue(final FuelInfoSpecification specification) {
+        return extractRoutingLength(specification);
     }
 
 }
