10c847894299da4b24ee0c4cdc927d61848794b0
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-14 11:09:40 +0300

    refactor FirstTableFuelInfoSearchingService

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/ploughing/byspecificresistance/FirstTableFuelInfoSearchingService.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/ploughing/byspecificresistance/FirstTableFuelInfoSearchingService.java
@@ -6,12 +6,9 @@ import org.springframework.stereotype.Service;
 @Service
 public final class FirstTableFuelInfoSearchingService extends AbstractPloughingFuelInfoSearchingBySpecificResistanceService {
     private static final String TABLE_NAME = "ВСПАШКА ПЛАСТА МНОГОЛЕТНИХ ТРАВ";
-    private static final String[] ROUTING_LENGTHS = new String[]{
-            "Менее 150", "150–200", "201–300", "301–400", "401–600", "601–1000", "Более 1000"
-    };
     private static final int FIRST_FUEL_INFO_OFFSET = 2;
 
     public FirstTableFuelInfoSearchingService(final FuelDocument fuelDocument) {
-        super(fuelDocument, TABLE_NAME, ROUTING_LENGTHS, FIRST_FUEL_INFO_OFFSET);
+        super(fuelDocument, TABLE_NAME, FIRST_FUEL_INFO_OFFSET);
     }
 }
