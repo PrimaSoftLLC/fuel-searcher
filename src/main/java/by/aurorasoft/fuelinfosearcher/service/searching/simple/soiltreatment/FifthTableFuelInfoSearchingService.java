96645ff40b93b9988d300f81957d58ccb9ee7ffe
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-14 11:15:30 +0300

    refactor FifthTableFuelInfoSearchingService

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/soiltreatment/FifthTableFuelInfoSearchingService.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/soiltreatment/FifthTableFuelInfoSearchingService.java
@@ -6,12 +6,9 @@ import org.springframework.stereotype.Service;
 @Service
 public final class FifthTableFuelInfoSearchingService extends AbstractSoilTreatmentFuelInfoSearchingService {
     private static final String TABLE_NAME = "ЛУЩЕНИЕ И ДИСКОВАНИЕ СТЕРНИ";
-    private static final String[] ROUTING_LENGTHS = new String[]{
-            "Менее 150", "150–200", "201–300", "301–400", "401–600", "601–1000", "Более 1000"
-    };
     private static final int FIRST_FUEL_INFO_OFFSET = 0;
 
     public FifthTableFuelInfoSearchingService(final FuelDocument fuelDocument) {
-        super(fuelDocument, TABLE_NAME, ROUTING_LENGTHS, FIRST_FUEL_INFO_OFFSET);
+        super(fuelDocument, TABLE_NAME, FIRST_FUEL_INFO_OFFSET);
     }
 }
