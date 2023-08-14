ca011014ac6ec6219a1e365442e6e7296865407d
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-14 11:21:42 +0300

    refactor TenthTableFuelInfoSearchingService

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/sowing/TenthTableFuelInfoSearchingService.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/sowing/TenthTableFuelInfoSearchingService.java
@@ -6,13 +6,10 @@ import org.springframework.stereotype.Service;
 @Service
 public final class TenthTableFuelInfoSearchingService extends AbstractSowingFuelInfoSearchingService {
     private static final String TABLE_NAME = "ПРЕДПОСЕВНАЯ ОБРАБОТКА ПОЧВЫ С ОДНОВРЕМЕННЫМ ПОСЕВОМ РАПСА";
-    private static final String[] ROUTING_LENGTHS = new String[]{
-            "Менее 150", "150–200", "201–300", "301–400", "401–600", "601–1000", "Более 1000"
-    };
     private static final int FIRST_FUEL_INFO_OFFSET = 0;
 
     public TenthTableFuelInfoSearchingService(final FuelDocument fuelDocument) {
-        super(fuelDocument, TABLE_NAME, ROUTING_LENGTHS, FIRST_FUEL_INFO_OFFSET);
+        super(fuelDocument, TABLE_NAME, FIRST_FUEL_INFO_OFFSET);
     }
 
 }
