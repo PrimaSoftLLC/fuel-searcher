03e17f94fa33ba3df6fbda47753ad8d5ef80ff8d
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-14 11:00:26 +0300

    refactor FifteenthTableFuelInfoSearchingService

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/movingandmoulding/moving/FifteenthTableFuelInfoSearchingService.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/movingandmoulding/moving/FifteenthTableFuelInfoSearchingService.java
@@ -6,13 +6,10 @@ import org.springframework.stereotype.Service;
 @Service
 public final class FifteenthTableFuelInfoSearchingService extends AbstractMovingTableFuelInfoSearchingService {
     private static final String TABLE_NAME = "КОШЕНИЕ СЕЯНЫХ И ЕСТЕСТВЕННЫХ ТРАВ";
-    private static final String[] ROUTING_LENGTHS = new String[]{
-            "Менее 150", "151…200", "201…300", "301…400", "401…600", "601…1000", "Более 1000"
-    };
     private static final int FIRST_FUEL_INFO_OFFSET = 0;
 
     public FifteenthTableFuelInfoSearchingService(final FuelDocument fuelDocument) {
-        super(fuelDocument, TABLE_NAME, ROUTING_LENGTHS, FIRST_FUEL_INFO_OFFSET);
+        super(fuelDocument, TABLE_NAME, FIRST_FUEL_INFO_OFFSET);
     }
 
 }
