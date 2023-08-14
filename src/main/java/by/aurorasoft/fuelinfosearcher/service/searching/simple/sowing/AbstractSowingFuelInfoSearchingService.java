fa6d112325fc825cb0e23df26a4f24b2e932c2c2
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-14 11:20:34 +0300

    refactor AbstractSowingFuelInfoSearchingService

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/sowing/AbstractSowingFuelInfoSearchingService.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/sowing/AbstractSowingFuelInfoSearchingService.java
@@ -9,15 +9,18 @@ import java.util.List;
 import java.util.Optional;
 
 import static by.aurorasoft.fuelinfosearcher.util.FuelDocumentRowFilterUtil.*;
+import static by.aurorasoft.fuelinfosearcher.util.FuelInfoSpecificationUtil.extractRoutingLength;
 
 public abstract class AbstractSowingFuelInfoSearchingService extends AbstractSimpleTableFuelInfoSearchingService {
+    private static final String[] FUEL_INFO_HEADERS = new String[]{
+            "Менее 150", "150–200", "201–300", "301–400", "401–600", "601–1000", "Более 1000"
+    };
     private static final int CELL_INDEX_MACHINERY = 2;
 
     public AbstractSowingFuelInfoSearchingService(final FuelDocument fuelDocument,
                                                   final String fuelTableName,
-                                                  final String[] routingLengths,
                                                   final int firstFuelInfoOffset) {
-        super(fuelDocument, fuelTableName, routingLengths, firstFuelInfoOffset);
+        super(fuelDocument, fuelTableName, FUEL_INFO_HEADERS, firstFuelInfoOffset);
     }
 
     @Override
@@ -27,6 +30,11 @@ public abstract class AbstractSowingFuelInfoSearchingService extends AbstractSim
                 .flatMap(rows -> findRowsByTractor(rows, specification))
                 .flatMap(rows -> findRowsByMachinery(rows, specification, CELL_INDEX_MACHINERY))
                 .flatMap(rows -> findRowByWorkingWidth(rows, specification));
+    }
+
+    @Override
+    protected final String extractFuelInfoHeaderCellValue(final FuelInfoSpecification specification) {
+        return extractRoutingLength(specification);
     }
 
 }
