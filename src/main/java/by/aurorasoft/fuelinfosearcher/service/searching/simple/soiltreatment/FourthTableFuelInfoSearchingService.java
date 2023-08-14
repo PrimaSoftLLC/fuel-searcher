c92b4ba27d854e14918d4e3dd3ecd691cd205b12
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-14 11:15:54 +0300

    refactor FourthTableFuelInfoSearchingService

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/soiltreatment/FourthTableFuelInfoSearchingService.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/simple/soiltreatment/FourthTableFuelInfoSearchingService.java
@@ -6,13 +6,10 @@ import org.springframework.stereotype.Service;
 @Service
 public final class FourthTableFuelInfoSearchingService extends AbstractSoilTreatmentFuelInfoSearchingService {
     private static final String TABLE_NAME = "СПЛОШНАЯ И КОМБИНИРОВАННАЯ ОБРАБОТКА ПОЧВЫ";
-    private static final String[] ROUTING_LENGTHS = new String[]{
-            "Менее 150", "150–200", "201–300", "301–400", "401–600", "601–1000", "Более 1000"
-    };
     private static final int FIRST_FUEL_INFO_OFFSET = 0;
 
     public FourthTableFuelInfoSearchingService(final FuelDocument fuelDocument) {
-        super(fuelDocument, TABLE_NAME, ROUTING_LENGTHS, FIRST_FUEL_INFO_OFFSET);
+        super(fuelDocument, TABLE_NAME, FIRST_FUEL_INFO_OFFSET);
     }
 
 }
