eca7c8ef4c8f2eef6afab390d4493ad5cddbad89
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-15 10:58:44 +0300

    add ThreePointsFuelDocumentCorrector

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/contentcorrecting/corrector/ThreePointsFuelDocumentCorrector.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/contentcorrecting/corrector/ThreePointsFuelDocumentCorrector.java
@@ -1,4 +1,18 @@
 package by.aurorasoft.fuelinfosearcher.service.contentcorrecting.corrector;
 
-public class ThreePointsFuelDocumentCorrector {
+import org.springframework.stereotype.Component;
+
+@Component
+public final class ThreePointsFuelDocumentCorrector extends AbstractFuelDocumentContentCorrector {
+    private static final String REPLACED_REGEX = "…";
+    private static final String REPLACEMENT = "...";
+
+    public ThreePointsFuelDocumentCorrector(final AbstractFuelDocumentContentCorrector nextCorrector) {
+        super(REPLACED_REGEX, nextCorrector);
+    }
+
+    @Override
+    protected String createReplacement(final String existing) {
+        return REPLACEMENT;
+    }
 }
