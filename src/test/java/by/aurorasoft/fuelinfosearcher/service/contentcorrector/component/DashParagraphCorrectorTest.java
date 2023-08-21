83b0ab8c38881db28d770182bab4fee7cb5bc2b7
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-22 00:50:50 +0300

    add NewLineParagraphCorrectorTest

--- src/test/java/by/aurorasoft/fuelinfosearcher/service/contentcorrector/component/DashParagraphCorrectorTest.java
+++ src/test/java/by/aurorasoft/fuelinfosearcher/service/contentcorrector/component/DashParagraphCorrectorTest.java
@@ -5,11 +5,11 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public final class DashParagraphCorrectorTest {
-    private final DashParagraphCorrector givenCorrector = new DashParagraphCorrector();
+    private final DashParagraphCorrector corrector = new DashParagraphCorrector();
 
     @Test
     public void replacementShouldBeCreated() {
-        final String actual = this.givenCorrector.createReplacement(null);
+        final String actual = this.corrector.createReplacement(null);
         final String expected = "-";
         assertEquals(expected, actual);
     }
--- src/test/java/by/aurorasoft/fuelinfosearcher/service/contentcorrector/component/NewLineParagraphCorrectorTest.java
+++ src/test/java/by/aurorasoft/fuelinfosearcher/service/contentcorrector/component/NewLineParagraphCorrectorTest.java
@@ -4,12 +4,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public final class NbspParagraphCorrectorTest {
-    private final NbspParagraphCorrector corrector = new NbspParagraphCorrector();
+public final class NewLineParagraphCorrectorTest {
+    private final NewLineParagraphCorrector corrector = new NewLineParagraphCorrector();
 
     @Test
     public void replacementShouldBeCreated() {
-        final String actual = corrector.createReplacement(null);
+        final String actual = this.corrector.createReplacement(null);
         final String expected = " ";
         assertEquals(expected, actual);
     }
