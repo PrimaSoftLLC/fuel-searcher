6b61f01568210d129ee4137c7f881c9d9ca28cf0
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-22 10:13:53 +0300

    correct arguments to test searching in table #27

--- src/test/java/by/aurorasoft/fuelinfosearcher/service/searching/it/argumentprovider/TwentySeventhTableFuelInfoSearchingArgumentsProvider.java
+++ src/test/java/by/aurorasoft/fuelinfosearcher/service/searching/it/argumentprovider/TwentySeventhTableFuelInfoSearchingArgumentsProvider.java
@@ -21,7 +21,7 @@ public final class TwentySeventhTableFuelInfoSearchingArgumentsProvider extends 
                                 .tractor("БЕЛАРУС-3522")
                                 .machinery("ПСС-25")
                                 .cargoClass("Грузы I класса")
-                                .transportDistance("0,25-0,75")
+                                .transportDistance("0.25-0.75")
                                 .roadGroup("I")
                                 .build(),
                         optionalFuelInfoFactory.apply(341.7, 0.19)
@@ -32,7 +32,7 @@ public final class TwentySeventhTableFuelInfoSearchingArgumentsProvider extends 
                                 .tractor("БЕЛАРУС-3522")
                                 .machinery("ПСС-20")
                                 .cargoClass("Грузы II класса")
-                                .transportDistance("3,26-4")
+                                .transportDistance("3.26-4")
                                 .roadGroup("II")
                                 .build(),
                         optionalFuelInfoFactory.apply(146.9, 0.83)
@@ -43,7 +43,7 @@ public final class TwentySeventhTableFuelInfoSearchingArgumentsProvider extends 
                                 .tractor("БЕЛАРУС-3522")
                                 .machinery("ПСТБ-17")
                                 .cargoClass("Грузы III класса")
-                                .transportDistance("0,25-0,75")
+                                .transportDistance("0.25-0.75")
                                 .roadGroup("III")
                                 .build(),
                         optionalFuelInfoFactory.apply(131.9, 0.48)
@@ -54,7 +54,7 @@ public final class TwentySeventhTableFuelInfoSearchingArgumentsProvider extends 
                                 .tractor("БЕЛАРУС-3022")
                                 .machinery("ПСС-25")
                                 .cargoClass("Грузы IV класса")
-                                .transportDistance("0,76-1,25")
+                                .transportDistance("0.76-1.25")
                                 .roadGroup("III")
                                 .build(),
                         optionalFuelInfoFactory.apply(91.9, 0.7)
@@ -65,7 +65,7 @@ public final class TwentySeventhTableFuelInfoSearchingArgumentsProvider extends 
                                 .tractor("БЕЛАРУС-3022")
                                 .machinery("ПСС-20")
                                 .cargoClass("Грузы II класса")
-                                .transportDistance("1,26-1,75")
+                                .transportDistance("1.26-1.75")
                                 .roadGroup("II")
                                 .build(),
                         optionalFuelInfoFactory.apply(201., 0.38)
@@ -77,7 +77,7 @@ public final class TwentySeventhTableFuelInfoSearchingArgumentsProvider extends 
                                 .tractor("Беларус-3022")
                                 .machinery("BAASTRUP 18")
                                 .cargoClass("Грузы III класса")
-                                .transportDistance("5,1-6")
+                                .transportDistance("5.1-6")
                                 .roadGroup("II")
                                 .build(),
                         optionalFuelInfoFactory.apply(54.2, 1.66)
@@ -88,7 +88,7 @@ public final class TwentySeventhTableFuelInfoSearchingArgumentsProvider extends 
                                 .tractor("БЕЛАРУС-1221")
                                 .machinery("СТС-12")
                                 .cargoClass("Грузы II класса")
-                                .transportDistance("3,26-4")
+                                .transportDistance("3.26-4")
                                 .roadGroup("I")
                                 .build(),
                         optionalFuelInfoFactory.apply(69.3, 0.8)
@@ -99,7 +99,7 @@ public final class TwentySeventhTableFuelInfoSearchingArgumentsProvider extends 
                                 .tractor("Беларус 920")
                                 .machinery("ПС-30")
                                 .cargoClass("Грузы I класса")
-                                .transportDistance("45,1-50")
+                                .transportDistance("45.1-50")
                                 .roadGroup("III")
                                 .build(),
                         optionalFuelInfoFactory.apply(8.7, 5.65)
@@ -111,7 +111,7 @@ public final class TwentySeventhTableFuelInfoSearchingArgumentsProvider extends 
                                 .tractor("not existing")
                                 .machinery("ПС-30")
                                 .cargoClass("Грузы I класса")
-                                .transportDistance("45,1-50")
+                                .transportDistance("45.1-50")
                                 .roadGroup("III")
                                 .build(),
                         empty()
@@ -123,7 +123,7 @@ public final class TwentySeventhTableFuelInfoSearchingArgumentsProvider extends 
                                 .tractor("Беларус 920")
                                 .machinery("not existing")
                                 .cargoClass("Грузы I класса")
-                                .transportDistance("45,1-50")
+                                .transportDistance("45.1-50")
                                 .roadGroup("III")
                                 .build(),
                         empty()
@@ -135,7 +135,7 @@ public final class TwentySeventhTableFuelInfoSearchingArgumentsProvider extends 
                                 .tractor("Беларус 920")
                                 .machinery("ПС-30")
                                 .cargoClass("not existing")
-                                .transportDistance("45,1-50")
+                                .transportDistance("45.1-50")
                                 .roadGroup("III")
                                 .build(),
                         empty()
@@ -159,7 +159,7 @@ public final class TwentySeventhTableFuelInfoSearchingArgumentsProvider extends 
                                 .tractor("Беларус 920")
                                 .machinery("ПС-30")
                                 .cargoClass("Грузы I класса")
-                                .transportDistance("45,1-50")
+                                .transportDistance("45.1-50")
                                 .roadGroup("not existing")
                                 .build(),
                         empty()
