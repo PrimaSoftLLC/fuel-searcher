3a50d90cc2002cf51e7a4df22862ae69e2cb0a9b
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-24 01:54:53 +0300

    refactor

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/manager/factory/searcherreader/SimpleTableFuelSearchersReader.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/manager/factory/searcherreader/SimpleTableFuelSearchersReader.java
@@ -3,11 +3,14 @@ package by.aurorasoft.fuelinfosearcher.service.searching.manager.factory.searche
 import by.aurorasoft.fuelinfosearcher.service.searching.manager.factory.searcherreader.streamreader.CloseableXMLStreamReader;
 import by.aurorasoft.fuelinfosearcher.service.searching.simple.SimpleTableFuelSearcher;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class SimpleTableFuelSearchersReader extends AbstractTableFuelSearchersReader<SimpleTableFuelSearcher> {
     private static final String TAG_NAME_SEARCHERS = "simple-fuel-tables";
-    private static final String TAG_NAME_FUEL_HEADERS = "fuel-headers";
-
     private static final String TAG_NAME_TABLE_NAME = "name";
+    private static final String TAG_NAME_FUEL_HEADERS = "fuel-headers";
+    private static final String TAG_NAME_FUEL_HEADER = "fuel-header";
 
     public SimpleTableFuelSearchersReader() {
         super(TAG_NAME_SEARCHERS);
@@ -15,17 +18,31 @@ public final class SimpleTableFuelSearchersReader extends AbstractTableFuelSearc
 
     @Override
     protected SimpleTableFuelSearcher readSearcher(final CloseableXMLStreamReader streamReader) {
-
+        streamReader.nextStartTag();   //to <name>
     }
 
     private static String readTableName(final CloseableXMLStreamReader streamReader) {
-        streamReader.skipUntilNextTagWithGivenName(TAG_NAME_TABLE_NAME);
-        return streamReader.findElementText();
+        final String tableName = streamReader.findTagText();
+        streamReader.nextStartTag();   //to <fuel-headers>
+        return tableName;
     }
 
+    //after this method 'streamReader' will point to <fuel-header-cell-property>
     private static String[] readFuelHeaders(final CloseableXMLStreamReader streamReader) {
-        streamReader.skipUntilNextTagWithGivenName(TAG_NAME_FUEL_HEADERS);
+        final List<String> fuelHeaders = new ArrayList<>();
+        streamReader.nextStartTag();   //to <fuel-header>
+        while (streamReader.isStartTag(TAG_NAME_FUEL_HEADER)) {
+            final String currentFuelHeader = streamReader.findTagText();
+            fuelHeaders.add(currentFuelHeader);
+            streamReader.nextStartTag();
+        }
+        return fuelHeaders.toArray(String[]::new);
+    }
 
+    private static String readFuelHeaderCellProperty(final CloseableXMLStreamReader streamReader) {
+        final String fuelHeaderCellProperty = streamReader.findTagText();
+        streamReader.nextStartTag();   //to <filter-chain>
+        return fuelHeaderCellProperty;
     }
 
 
--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/manager/factory/searcherreader/streamreader/CloseableXMLStreamReader.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/manager/factory/searcherreader/streamreader/CloseableXMLStreamReader.java
@@ -12,8 +12,10 @@ import java.io.InputStream;
 import java.util.Objects;
 
 import static javax.xml.stream.XMLInputFactory.newFactory;
+import static javax.xml.stream.XMLStreamConstants.END_ELEMENT;
 import static javax.xml.stream.XMLStreamConstants.START_ELEMENT;
 
+//TODO: сделать так чтобы все было на уровне тегов
 public final class CloseableXMLStreamReader implements Closeable {
     private final XMLStreamReader xmlStreamReader;
 
@@ -37,6 +39,14 @@ public final class CloseableXMLStreamReader implements Closeable {
         }
     }
 
+    public void nextTag() {
+        try {
+            this.xmlStreamReader.nextTag();
+        } catch (final XMLStreamException cause) {
+            throw new XMLReadingException(cause);
+        }
+    }
+
     public int findEventType() {
         return this.xmlStreamReader.getEventType();
     }
@@ -45,7 +55,7 @@ public final class CloseableXMLStreamReader implements Closeable {
         return this.xmlStreamReader.getLocalName();
     }
 
-    public String findElementText() {
+    public String findTagText() {
         try {
             return this.xmlStreamReader.getElementText();
         } catch (final XMLStreamException cause) {
@@ -53,13 +63,35 @@ public final class CloseableXMLStreamReader implements Closeable {
         }
     }
 
-    public void skipUntilNextTagWithGivenName(final String tagName) {
-        while (!this.isCurrentStartTagWithGivenName(tagName)) {
-            this.next();
+    public void nextStartTag() {
+        while (!this.isCurrentStartTag()) {
+            this.nextTag();
         }
     }
 
+    public void skipUntilNextTag() {
+        this.nextTag();
+    }
 
+    public void skipUntilStartTag(final String tagName) {
+        while (!this.isStartTag(tagName)) {
+            this.nextTag();
+        }
+    }
+
+    public boolean isCurrentStartTag() {
+        return this.findEventType() == START_ELEMENT;
+    }
+
+    //TODO: refactor
+    public boolean isStartTag(final String tagName) {
+        return this.findEventType() == START_ELEMENT && Objects.equals(this.findLocalName(), tagName);
+    }
+
+    //TODO: refactor
+    public boolean isEndTag(final String tagName) {
+        return this.findEventType() == END_ELEMENT && Objects.equals(this.findLocalName(), tagName);
+    }
 
     @Override
     public void close() {
@@ -78,9 +110,5 @@ public final class CloseableXMLStreamReader implements Closeable {
         } catch (final FileNotFoundException | XMLStreamException cause) {
             throw new XMLReadingException(cause);
         }
-    }
-
-    private boolean isCurrentStartTagWithGivenName(final String elementName) {
-        return this.findEventType() == START_ELEMENT && Objects.equals(this.findLocalName(), elementName);
     }
 }
