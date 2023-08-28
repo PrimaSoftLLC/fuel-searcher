5aba052ed3007e04254e870969ed2c3681655351
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-28 15:26:27 +0300

    add configuration and searching for tables #7

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/manager/factory/searchersreader/handler/dictionary/rowfilter/interim/GroupFilterFactoryDictionary.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searching/manager/factory/searchersreader/handler/dictionary/rowfilter/interim/GroupFilterFactoryDictionary.java
@@ -3,6 +3,7 @@ package by.aurorasoft.fuelinfosearcher.service.searching.manager.factory.searche
 import by.aurorasoft.fuelinfosearcher.functionalinterface.filterfactory.interim.GroupFilterFactory;
 import by.aurorasoft.fuelinfosearcher.service.searching.rowfilter.intermediate.group.ProcessingDepthRowFilter;
 import by.aurorasoft.fuelinfosearcher.service.searching.rowfilter.intermediate.group.SoilTypeRowFilter;
+import by.aurorasoft.fuelinfosearcher.service.searching.rowfilter.intermediate.group.SowingNormRowFilter;
 import by.aurorasoft.fuelinfosearcher.service.searching.rowfilter.intermediate.group.SpecificResistanceRowFilter;
 import org.springframework.stereotype.Component;
 
@@ -18,7 +19,8 @@ public final class GroupFilterFactoryDictionary extends InterimFilterFactoryDict
     private static final Map<String, GroupFilterFactory> FACTORIES_BY_KEYS = ofEntries(
             entry("удельное сопротивление", SpecificResistanceRowFilter::new),
             entry("тип почвы", SoilTypeRowFilter::new),
-            entry("глубина обработки", ProcessingDepthRowFilter::new)
+            entry("глубина обработки", ProcessingDepthRowFilter::new),
+            entry("норма высева", SowingNormRowFilter::new)
     );
 
     public GroupFilterFactoryDictionary() {
