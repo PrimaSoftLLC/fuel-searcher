6f1c652f3f158e91b8a710b69e90f1f872334618
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-29 16:43:09 +0300

    refactor FuelHeaderNameTagHandler

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searcher/manager/factory/searchersreader/handler/taghandler/translating/fuelheadername/FuelHeaderNameTagHandler.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searcher/manager/factory/searchersreader/handler/taghandler/translating/fuelheadername/FuelHeaderNameTagHandler.java
@@ -3,12 +3,12 @@ package by.aurorasoft.fuelinfosearcher.service.searcher.manager.factory.searcher
 import by.aurorasoft.fuelinfosearcher.model.FuelHeaderMetadata;
 import by.aurorasoft.fuelinfosearcher.service.searcher.manager.factory.searchersreader.handler.context.SearchersParsingContext;
 import by.aurorasoft.fuelinfosearcher.service.searcher.manager.factory.searchersreader.handler.dictionary.FuelHeaderMetadataDictionary;
-import by.aurorasoft.fuelinfosearcher.service.searcher.manager.factory.searchersreader.handler.taghandler.translating.TranslatingTagHandler;
+import by.aurorasoft.fuelinfosearcher.service.searcher.manager.factory.searchersreader.handler.taghandler.translating.SimpleTranslatingTagHandler;
 import by.aurorasoft.fuelinfosearcher.service.searcher.manager.factory.searchersreader.handler.taghandler.translating.fuelheadername.exception.NoSuchFuelHeaderNameException;
 import org.springframework.stereotype.Component;
 
 @Component
-public final class FuelHeaderNameTagHandler extends TranslatingTagHandler<FuelHeaderMetadata> {
+public final class FuelHeaderNameTagHandler extends SimpleTranslatingTagHandler<FuelHeaderMetadata> {
     private static final String TAG_NAME = "fuel-header-name";
 
     public FuelHeaderNameTagHandler(final FuelHeaderMetadataDictionary dictionary) {
@@ -22,7 +22,13 @@ public final class FuelHeaderNameTagHandler extends TranslatingTagHandler<FuelHe
     }
 
     @Override
-    protected void accumulateTranslatedValue(final SearchersParsingContext context, final FuelHeaderMetadata headerMetadata) {
+    protected void accumulateTranslatedValue(final SearchersParsingContext context,
+                                             final FuelHeaderMetadata headerMetadata) {
         context.accumulateFuelHeaderMetaData(headerMetadata);
+    }
+
+    @Override
+    protected void accumulateAdditionalValues(final SearchersParsingContext context) {
+
     }
 }
