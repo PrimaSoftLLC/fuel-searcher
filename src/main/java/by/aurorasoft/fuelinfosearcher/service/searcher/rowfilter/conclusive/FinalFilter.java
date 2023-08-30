7c1b886657c457674f3747998a0fa7f55c33f169
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-30 12:19:45 +0300

    refactor FinalFilter

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searcher/rowfilter/conclusive/FinalFilter.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searcher/rowfilter/conclusive/FinalFilter.java
@@ -1,6 +1,6 @@
 package by.aurorasoft.fuelinfosearcher.service.searcher.rowfilter.conclusive;
 
-import by.aurorasoft.fuelinfosearcher.functionalinterface.filteringfunction.FilteringFunction;
+import by.aurorasoft.fuelinfosearcher.functionalinterface.filteringfunction.FinalFilteringFunction;
 import by.aurorasoft.fuelinfosearcher.service.searcher.rowfilter.Filter;
 import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
@@ -9,11 +9,10 @@ import java.util.Optional;
 
 import static by.aurorasoft.fuelinfosearcher.util.XWPFUtil.findFirstRowByContent;
 
-//TODO: remove FuelInfoSpecificationUtil
-public abstract class FinalFilter extends Filter<Optional<XWPFTableRow>> {
+public abstract class FinalFilter extends Filter<Optional<XWPFTableRow>, FinalFilteringFunction> {
 
     public FinalFilter(final int filteringCellIndex) {
-        super(filteringCellIndex);
+        super(filteringCellIndex, FinalFilteringFunction.class);
     }
 
     @Override
