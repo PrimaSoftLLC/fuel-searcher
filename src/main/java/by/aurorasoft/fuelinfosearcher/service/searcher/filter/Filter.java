7442b9749cd587a68f2f5c1077e19d808aa3fe2b
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-31 11:49:14 +0300

    refactor Filter

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searcher/filter/Filter.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searcher/filter/Filter.java
@@ -1,6 +1,7 @@
 package by.aurorasoft.fuelinfosearcher.service.searcher.filter;
 
 import by.aurorasoft.fuelinfosearcher.model.specification.Specification;
+import by.aurorasoft.fuelinfosearcher.model.specification.propertyextractor.SpecificationPropertyExtractor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.apache.poi.xwpf.usermodel.XWPFTableRow;
@@ -10,16 +11,15 @@ import java.util.List;
 @RequiredArgsConstructor
 @Getter
 public abstract class Filter<R> {
+    private final SpecificationPropertyExtractor filtrationValueExtractor;
     private final int filtrationCellIndex;
 
     public final R filter(final List<XWPFTableRow> rows, final Specification specification) {
-        final String filtrationValue = this.extractFiltrationValue(specification);
+        final String filtrationValue = this.filtrationValueExtractor.extractProperty(specification);
         return this.filter(rows, filtrationValue, this.filtrationCellIndex);
     }
 
     protected abstract R filter(final List<XWPFTableRow> rows,
                                 final String filtrationValue,
                                 final int filtrationCellIndex);
-
-    protected abstract String extractFiltrationValue(final Specification specification);
 }
