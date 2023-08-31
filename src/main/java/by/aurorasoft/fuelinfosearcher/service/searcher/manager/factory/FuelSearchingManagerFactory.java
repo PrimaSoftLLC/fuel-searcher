33b175609e849663ba8cd30f3e0f9b4ac572191f
Vlad Zuev <vladzuev.00@mail.ru>
2023-08-31 11:47:46 +0300

    refactor FuelSearchingManagerFactory

--- src/main/java/by/aurorasoft/fuelinfosearcher/service/searcher/manager/factory/FuelSearchingManagerFactory.java
+++ src/main/java/by/aurorasoft/fuelinfosearcher/service/searcher/manager/factory/FuelSearchingManagerFactory.java
@@ -1,5 +1,6 @@
 package by.aurorasoft.fuelinfosearcher.service.searcher.manager.factory;
 
+import by.aurorasoft.fuelinfosearcher.model.specification.propertyextractor.TableNameExtractor;
 import by.aurorasoft.fuelinfosearcher.service.searcher.FuelSearcher;
 import by.aurorasoft.fuelinfosearcher.service.searcher.manager.FuelSearchingManager;
 import by.aurorasoft.fuelinfosearcher.service.searcher.manager.factory.searchersreader.FuelSearchersReader;
@@ -12,9 +13,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public final class FuelSearchingManagerFactory {
     private final FuelSearchersReader fuelSearchersReader;
+    private final TableNameExtractor tableNameExtractor;
 
     public FuelSearchingManager create(final String filePath) {
         final List<FuelSearcher> searchers = this.fuelSearchersReader.read(filePath);
-        return new FuelSearchingManager(searchers);
+        return new FuelSearchingManager(searchers, this.tableNameExtractor);
     }
 }
