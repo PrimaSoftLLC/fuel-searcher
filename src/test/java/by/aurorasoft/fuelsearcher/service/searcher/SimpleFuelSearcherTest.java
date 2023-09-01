713cb8ca5e1fc3b8da979b41d53e25cb6a35875a
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-01 11:10:29 +0300

    refactor SimpleFuelSearcherTest

--- src/test/java/by/aurorasoft/fuelsearcher/service/searcher/SimpleFuelSearcherTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/service/searcher/SimpleFuelSearcherTest.java
@@ -3,15 +3,20 @@ package by.aurorasoft.fuelsearcher.service.searcher;
 import by.aurorasoft.fuelsearcher.model.FuelHeaderMetadata;
 import by.aurorasoft.fuelsearcher.model.FuelTable;
 import by.aurorasoft.fuelsearcher.model.specification.Specification;
+import by.aurorasoft.fuelsearcher.service.searcher.SimpleFuelSearcher.SimpleSearcherBuilder;
 import by.aurorasoft.fuelsearcher.service.searcher.filterchain.FilterChain;
 import org.apache.poi.xwpf.usermodel.IBodyElement;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.junit.Test;
 
 import java.lang.reflect.Constructor;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
 public final class SimpleFuelSearcherTest {
@@ -21,11 +26,98 @@ public final class SimpleFuelSearcherTest {
             throws Exception {
         final SimpleFuelSearcher givenSearcher = createSearcher();
 
-        final IBodyElement firstElement = mock(IBodyElement.class);
-        final List<IBodyElement> givenElements = List.of(firstElement, mock(IBodyElement.class));
+        final XWPFTable element = mock(XWPFTable.class);
+        final List<IBodyElement> givenElements = singletonList(element);
         final Specification givenSpecification = mock(Specification.class);
 
-        final IBodyElement actual = 
+        final Optional<XWPFTable> optionalActual = givenSearcher.findSubTable(givenElements, givenSpecification);
+        assertTrue(optionalActual.isPresent());
+        final XWPFTable actual = optionalActual.get();
+        assertSame(element, actual);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void subTableShouldNotBeFoundBecauseOfElementsIsEmpty()
+            throws Exception {
+        final SimpleFuelSearcher givenSearcher = createSearcher();
+
+        final List<IBodyElement> givenElements = emptyList();
+        final Specification givenSpecification = mock(Specification.class);
+
+        givenSearcher.findSubTable(givenElements, givenSpecification);
+    }
+
+    @Test(expected = ClassCastException.class)
+    public void subTableShouldNotBeFoundBecauseOfFirstElementIsNotTable()
+            throws Exception {
+        final SimpleFuelSearcher givenSearcher = createSearcher();
+
+        final List<IBodyElement> givenElements = List.of(mock(IBodyElement.class));
+        final Specification givenSpecification = mock(Specification.class);
+
+        givenSearcher.findSubTable(givenElements, givenSpecification);
+    }
+
+    @Test
+    public void builderShouldBeCreated() {
+        final SimpleSearcherBuilder actual = SimpleFuelSearcher.builder();
+        assertNotNull(actual);
+    }
+
+    @Test
+    public void elementsShouldBeValid() {
+        final SimpleSearcherBuilder givenBuilder = SimpleFuelSearcher.builder();
+        final List<IBodyElement> givenElements = List.of(mock(XWPFTable.class));
+
+        final boolean actual = givenBuilder.isValidElements(givenElements);
+        assertTrue(actual);
+    }
+
+    @Test
+    public void elementsShouldNotBeValidBecauseOfNotValidSize() {
+        final SimpleSearcherBuilder givenBuilder = SimpleFuelSearcher.builder();
+        final List<IBodyElement> givenElements = List.of(mock(XWPFTable.class), mock(IBodyElement.class));
+
+        final boolean actual = givenBuilder.isValidElements(givenElements);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void elementsShouldNotBeValidBecauseOfFirstElementIsNotTable() {
+        final SimpleSearcherBuilder givenBuilder = SimpleFuelSearcher.builder();
+        final List<IBodyElement> givenElements = List.of(mock(IBodyElement.class));
+
+        final boolean actual = givenBuilder.isValidElements(givenElements);
+        assertFalse(actual);
+    }
+
+    @Test
+    public void notValidElementsMessageShouldBeFound() {
+        final SimpleSearcherBuilder givenBuilder = SimpleFuelSearcher.builder();
+
+        final String actual = givenBuilder.findNotValidElementsMessage();
+        final String expected = "Fuel table should contain only one table-element";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void searcherShouldBeBuilt() {
+        final SimpleSearcherBuilder givenBuilder = SimpleFuelSearcher.builder();
+        final FuelTable givenFuelTable = mock(FuelTable.class);
+        final FuelHeaderMetadata givenFuelHeaderMetadata = mock(FuelHeaderMetadata.class);
+        final FilterChain givenFilterChain = mock(FilterChain.class);
+
+        final SimpleFuelSearcher actual = givenBuilder.build(givenFuelTable, givenFuelHeaderMetadata, givenFilterChain);
+        assertNotNull(actual);
+    }
+
+    @Test
+    public void builderAdditionalPropertiesShouldBeFound() {
+        final SimpleSearcherBuilder givenBuilder = SimpleFuelSearcher.builder();
+
+        final Stream<Object> actual = givenBuilder.findAdditionalProperties();
+        final boolean actualEmpty = actual.findFirst().isEmpty();
+        assertTrue(actualEmpty);
     }
 
     private static SimpleFuelSearcher createSearcher()
@@ -35,14 +127,14 @@ public final class SimpleFuelSearcherTest {
         );
         constructor.setAccessible(true);
         try {
-            final FuelHeaderMetadata fuelHeaderMetadata = createFuelHeaderMetaData();
+            final FuelHeaderMetadata fuelHeaderMetadata = createDefaultFuelHeaderMetaData();
             return constructor.newInstance(null, fuelHeaderMetadata, null);
         } finally {
             constructor.setAccessible(false);
         }
     }
 
-    private static FuelHeaderMetadata createFuelHeaderMetaData() {
+    private static FuelHeaderMetadata createDefaultFuelHeaderMetaData() {
         return FuelHeaderMetadata.builder()
                 .name("fuel-header")
                 .values(emptyList())
