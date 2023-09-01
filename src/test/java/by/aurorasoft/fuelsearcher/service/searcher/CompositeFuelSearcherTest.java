50fc5c39e1481fede7b22166acee5c4c47930a5e
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-01 12:59:42 +0300

    refactor CompositeFuelSearcher

--- src/test/java/by/aurorasoft/fuelsearcher/service/searcher/CompositeFuelSearcherTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/service/searcher/CompositeFuelSearcherTest.java
@@ -1,4 +1,21 @@
 package by.aurorasoft.fuelsearcher.service.searcher;
 
+import by.aurorasoft.fuelsearcher.service.searcher.CompositeFuelSearcher.CompositeSearcherBuilder;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
 public final class CompositeFuelSearcherTest {
+
+    @Test
+    public void builderShouldBeCreated() {
+        final CompositeSearcherBuilder actual = CompositeFuelSearcher.builder();
+        assertNotNull(actual);
+    }
+
+    @Test
+    public void subTableShouldBeFound() {
+        throw new RuntimeException();
+    }
+
 }
