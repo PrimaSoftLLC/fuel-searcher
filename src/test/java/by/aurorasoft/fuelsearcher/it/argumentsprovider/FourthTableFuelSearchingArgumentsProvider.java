e1171133a85a47be2c6aeeeed0271d6f29695b11
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-04 15:12:04 +0300

    add it searching for Table #4

--- src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/FourthTableFuelSearchingArgumentsProvider.java
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/FourthTableFuelSearchingArgumentsProvider.java
@@ -1,135 +1,126 @@
-package by.aurorasoft.fuelsearcher.service.searcher.it.argumentprovider;
+package by.aurorasoft.fuelsearcher.it.argumentsprovider;
 
+import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.FuelSearchingArguments;
 import by.aurorasoft.fuelsearcher.model.Fuel;
 import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification;
-import org.junit.jupiter.params.provider.Arguments;
 
-import java.util.Optional;
-import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
-import static java.util.Optional.empty;
-
-public final class FourthTableFuelSearchingArgumentsProvider extends AbstractTableFuelSearchingArgumentsProvider {
+public final class FourthTableFuelSearchingArgumentsProvider extends TableFuelSearchingArgumentsProvider {
 
     @Override
-    protected Stream<Arguments> provide(final BiFunction<Double, Double, Optional<Fuel>> optionalFuelFactory) {
+    protected Stream<FuelSearchingArguments> createFuelSearchingArguments() {
         return Stream.of(
-                Arguments.of(
-                        FuelSpecification.builder()
+                FuelSearchingArguments.builder()
+                        .specification(FuelSpecification.builder()
                                 .tableName("СПЛОШНАЯ И КОМБИНИРОВАННАЯ ОБРАБОТКА ПОЧВЫ")
                                 .tractor("Clas Xerion 5000")
                                 .machinery("Köckerling Allrounder 1200")
                                 .workingWidth("12")
                                 .routingLength("Менее 150")
                                 .processingDepth("Глубина обработки 6...8 см")
-                                .build(),
-                        optionalFuelFactory.apply(26.4, 14.1)
-                ),
-                Arguments.of(
-                        FuelSpecification.builder()
+                                .build())
+                        .expected(new Fuel(26.4, 14.1))
+                        .build(),
+                FuelSearchingArguments.builder()
+                        .specification(FuelSpecification.builder()
                                 .tableName("СПЛОШНАЯ И КОМБИНИРОВАННАЯ ОБРАБОТКА ПОЧВЫ")
                                 .tractor("Clas Xerion 5000")
                                 .machinery("Köckerling Allrounder 1200")
                                 .workingWidth("12")
                                 .routingLength("Более 1000")
                                 .processingDepth("Глубина обработки 6...8 см")
-                                .build(),
-                        optionalFuelFactory.apply(58.2, 7.5)
-                ),
-                Arguments.of(
-                        FuelSpecification.builder()
+                                .build())
+                        .expected(new Fuel(58.2, 7.5))
+                        .build(),
+                FuelSearchingArguments.builder()
+                        .specification(FuelSpecification.builder()
                                 .tableName("СПЛОШНАЯ И КОМБИНИРОВАННАЯ ОБРАБОТКА ПОЧВЫ")
                                 .tractor("Беларус 3522")
                                 .machinery("АПМ-8")
                                 .workingWidth("8")
                                 .routingLength("150-200")
                                 .processingDepth("Глубина обработки 6...8 см")
-                                .build(),
-                        optionalFuelFactory.apply(24.0, 11.1)
-                ),
-                Arguments.of(
-                        FuelSpecification.builder()
+                                .build())
+                        .expected(new Fuel(24.0, 11.1))
+                        .build(),
+                FuelSearchingArguments.builder()
+                        .specification(FuelSpecification.builder()
                                 .tableName("СПЛОШНАЯ И КОМБИНИРОВАННАЯ ОБРАБОТКА ПОЧВЫ")
                                 .tractor("Беларус 3022")
                                 .machinery("КФУ-7.3")
                                 .workingWidth("7.3")
                                 .routingLength("Менее 150")
                                 .processingDepth("Глубина обработки 6...8 см")
-                                .build(),
-                        optionalFuelFactory.apply(17.3, 12.7)
-                ),
-                Arguments.of(
-                        FuelSpecification.builder()
+                                .build())
+                        .expected(new Fuel(17.3, 12.7))
+                        .build(),
+                FuelSearchingArguments.builder()
+                        .specification(FuelSpecification.builder()
                                 .tableName("СПЛОШНАЯ И КОМБИНИРОВАННАЯ ОБРАБОТКА ПОЧВЫ")
                                 .tractor("Беларус 1522")
                                 .machinery("Horsch Terrano 4FX")
                                 .workingWidth("4")
                                 .routingLength("Менее 150")
                                 .processingDepth("Глубина обработки 14...20 см")
-                                .build(),
-                        optionalFuelFactory.apply(10.5, 8.7)
-                ),
+                                .build())
+                        .expected(new Fuel(10.5, 8.7))
+                        .build(),
                 //not existing tractor
-                Arguments.of(
-                        FuelSpecification.builder()
+                FuelSearchingArguments.builder()
+                        .specification(FuelSpecification.builder()
                                 .tableName("СПЛОШНАЯ И КОМБИНИРОВАННАЯ ОБРАБОТКА ПОЧВЫ")
                                 .tractor("not existing")
                                 .machinery("Horsch Terrano 4FX")
                                 .workingWidth("4")
                                 .routingLength("Менее 150")
                                 .processingDepth("Глубина обработки 14…20 см")
-                                .build(),
-                        empty()
-                ),
+                                .build())
+                        .build(),
                 //not existing machinery
-                Arguments.of(
-                        FuelSpecification.builder()
+                FuelSearchingArguments.builder()
+                        .specification(FuelSpecification.builder()
                                 .tableName("СПЛОШНАЯ И КОМБИНИРОВАННАЯ ОБРАБОТКА ПОЧВЫ")
                                 .tractor("Беларус 1522")
                                 .machinery("not existing")
                                 .workingWidth("4")
                                 .routingLength("Менее 150")
                                 .processingDepth("Глубина обработки 14...20 см")
-                                .build(),
-                        empty()
-                ),
+                                .build())
+                        .build(),
                 //not existing working width
-                Arguments.of(
-                        FuelSpecification.builder()
+                FuelSearchingArguments.builder()
+                        .specification(FuelSpecification.builder()
                                 .tableName("СПЛОШНАЯ И КОМБИНИРОВАННАЯ ОБРАБОТКА ПОЧВЫ")
                                 .tractor("Беларус 1522")
                                 .machinery("Horsch Terrano 4FX")
                                 .workingWidth("not existing")
                                 .routingLength("Менее 150")
                                 .processingDepth("Глубина обработки 14...20 см")
-                                .build(),
-                        empty()
-                ),
+                                .build())
+                        .build(),
                 //not existing routing length
-                Arguments.of(
-                        FuelSpecification.builder()
+                FuelSearchingArguments.builder()
+                        .specification(FuelSpecification.builder()
                                 .tableName("СПЛОШНАЯ И КОМБИНИРОВАННАЯ ОБРАБОТКА ПОЧВЫ")
                                 .tractor("Беларус 1522")
                                 .machinery("Horsch Terrano 4FX")
                                 .workingWidth("4")
                                 .routingLength("not existing")
                                 .processingDepth("Глубина обработки 14...20 см")
-                                .build(),
-                        empty()
-                ),
+                                .build())
+                        .build(),
                 //not existing processing depth
-                Arguments.of(
-                        FuelSpecification.builder()
+                FuelSearchingArguments.builder()
+                        .specification(FuelSpecification.builder()
                                 .tableName("СПЛОШНАЯ И КОМБИНИРОВАННАЯ ОБРАБОТКА ПОЧВЫ")
                                 .tractor("Беларус 1522")
                                 .machinery("Horsch Terrano 4FX")
                                 .workingWidth("4")
                                 .routingLength("Менее 150")
                                 .processingDepth("not existing")
-                                .build(),
-                        empty()
-                )
+                                .build())
+                        .build()
         );
     }
 
