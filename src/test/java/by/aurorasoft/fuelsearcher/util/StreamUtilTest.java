078d06b23a5a0c6c4b8f4abb51fe5c0d1138bbbd
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-04 16:01:31 +0300

    add StreamUtilTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/util/StreamUtilTest.java
@@ -0,0 +1,56 @@
+package by.aurorasoft.fuelsearcher.util;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static by.aurorasoft.fuelsearcher.util.StreamUtil.asStream;
+import static org.junit.Assert.*;
+
+public final class StreamUtilTest {
+
+    @Test
+    public void iteratorShouldBeConvertedToStream() {
+        final Iterator<Integer> givenIterator = new IntegerIterator(1, 2, 3);
+
+        final Stream<Integer> actual = asStream(givenIterator);
+        assertFalse(actual.isParallel());
+
+        final List<Integer> actualAsList = actual.toList();
+        final List<Integer> expectedAsList = List.of(1, 2, 3);
+        assertEquals(expectedAsList, actualAsList);
+    }
+
+    @Test
+    public void iteratorShouldBeConvertedToParallelStream() {
+        final Iterator<Integer> givenIterator = new IntegerIterator(1, 2, 3);
+
+        final Stream<Integer> actual = asStream(givenIterator, true);
+        assertTrue(actual.isParallel());
+
+        final List<Integer> actualAsList = actual.toList();
+        final List<Integer> expectedAsList = List.of(1, 2, 3);
+        assertEquals(expectedAsList, actualAsList);
+    }
+
+    private static final class IntegerIterator implements Iterator<Integer> {
+        private final Integer[] numbers;
+        private int nextNumberIndex;
+
+        public IntegerIterator(final Integer... numbers) {
+            this.numbers = numbers;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.nextNumberIndex < this.numbers.length;
+        }
+
+        @Override
+        public Integer next() {
+            return this.numbers[this.nextNumberIndex++];
+        }
+    }
+}
