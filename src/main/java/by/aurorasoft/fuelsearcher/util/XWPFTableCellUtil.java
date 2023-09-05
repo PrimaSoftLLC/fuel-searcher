a3246a8dfb40119b1b7b7f984b5713778cf76e38
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-05 13:01:33 +0300

    add XWPFTableCellUtil::extractContent and tests for it

--- src/main/java/by/aurorasoft/fuelsearcher/util/XWPFTableCellUtil.java
+++ src/main/java/by/aurorasoft/fuelsearcher/util/XWPFTableCellUtil.java
@@ -3,13 +3,23 @@ package by.aurorasoft.fuelsearcher.util;
 import lombok.experimental.UtilityClass;
 import org.apache.poi.xwpf.usermodel.XWPFTableCell;
 
+import static java.util.stream.Collectors.joining;
+
 @UtilityClass
 public final class XWPFTableCellUtil {
+    private static final String PARAGRAPH_CONTENT_SEPARATOR = " ";
 
     public static boolean isEmpty(final XWPFTableCell cell) {
         return cell.getParagraphs()
                 .stream()
                 .allMatch(XWPFParagraphUtil::isEmpty);
+    }
+
+    public static String extractContent(final XWPFTableCell cell) {
+        return cell.getParagraphs()
+                .stream()
+                .map(XWPFParagraphUtil::extractText)
+                .collect(joining(PARAGRAPH_CONTENT_SEPARATOR));
     }
 
 }
--- src/test/java/by/aurorasoft/fuelsearcher/util/XWPFTableCellUtilTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/util/XWPFTableCellUtilTest.java
@@ -7,10 +7,10 @@ import org.mockito.MockedStatic;
 
 import java.util.List;
 
+import static by.aurorasoft.fuelsearcher.util.XWPFTableCellUtil.extractContent;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public final class XWPFTableCellUtilTest {
@@ -53,6 +53,34 @@ public final class XWPFTableCellUtilTest {
         assertTrue(actual);
     }
 
+    @Test
+    public void cellContentShouldBeExtracted() {
+        final XWPFParagraph firstGivenParagraph = mock(XWPFParagraph.class);
+        final XWPFParagraph secondGivenParagraph = mock(XWPFParagraph.class);
+        final XWPFTableCell givenCell = createCell(firstGivenParagraph, secondGivenParagraph);
+
+        final String givenContentFirstParagraph = "content-1";
+        final String givenContentSecondParagraph = "content-2";
+
+        try (final MockedStatic<XWPFParagraphUtil> mockedParagraphUtil = mockStatic(XWPFParagraphUtil.class)) {
+            bindContentWithParagraph(firstGivenParagraph, givenContentFirstParagraph, mockedParagraphUtil);
+            bindContentWithParagraph(secondGivenParagraph, givenContentSecondParagraph, mockedParagraphUtil);
+
+            final String actual = extractContent(givenCell);
+            final String expected = "content-1 content-2";
+            assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void contentOfCellWithoutParagraphsShouldBeExtracted() {
+        final XWPFTableCell givenCell = createCell();
+
+        final String actual = extractContent(givenCell);
+        final String expected = "";
+        assertEquals(expected, actual);
+    }
+
     private static XWPFTableCell createCell(final XWPFParagraph... paragraphs) {
         final List<XWPFParagraph> paragraphsAsList = paragraphs != null ? asList(paragraphs) : emptyList();
         final XWPFTableCell cell = mock(XWPFTableCell.class);
@@ -74,5 +102,11 @@ public final class XWPFTableCellUtilTest {
                                              final MockedStatic<XWPFParagraphUtil> mockedParagraphUtil,
                                              final boolean empty) {
         mockedParagraphUtil.when(() -> XWPFParagraphUtil.isEmpty(same(paragraph))).thenReturn(empty);
+    }
+
+    private static void bindContentWithParagraph(final XWPFParagraph paragraph,
+                                                 final String content,
+                                                 final MockedStatic<XWPFParagraphUtil> mockedParagraphUtil) {
+        mockedParagraphUtil.when(() -> XWPFParagraphUtil.extractText(same(paragraph))).thenReturn(content);
     }
 }
