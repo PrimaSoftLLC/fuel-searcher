62dbde2faf510a03129058a2c01bfe4188690091
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-05 12:16:40 +0300

    refactor XWPFTableCellUtil

--- src/main/java/by/aurorasoft/fuelsearcher/util/XWPFTableCellUtil.java
+++ src/main/java/by/aurorasoft/fuelsearcher/util/XWPFTableCellUtil.java
@@ -3,13 +3,13 @@ package by.aurorasoft.fuelsearcher.util;
 import lombok.experimental.UtilityClass;
 import org.apache.poi.xwpf.usermodel.XWPFTableCell;
 
-//TODO: refactor
 @UtilityClass
 public final class XWPFTableCellUtil {
 
     public static boolean isEmpty(final XWPFTableCell cell) {
-        //TODO: extract paragraphs and use XWPFParagraphUtil.isEmpty
-        return cell.getText() == null || cell.getText().equals("");
+        return cell.getParagraphs()
+                .stream()
+                .allMatch(XWPFParagraphUtil::isEmpty);
     }
 
 }
