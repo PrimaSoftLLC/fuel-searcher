19e29d52ab798b415728ae00a3fa64f73228553e
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-05 22:36:41 +0300

    refactor SeventhTableFuelSearchingArgumentsProvider

--- src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/SeventhTableFuelSearchingArgumentsProvider.java
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/SeventhTableFuelSearchingArgumentsProvider.java
@@ -2,18 +2,25 @@ package by.aurorasoft.fuelsearcher.it.argumentsprovider;
 
 import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.FuelSearchingArguments;
 import by.aurorasoft.fuelsearcher.model.Fuel;
-import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification;
+import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification.FuelSpecificationBuilder;
 
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public final class SeventhTableFuelSearchingArgumentsProvider extends TableFuelSearchingArgumentsProvider {
+    private static final String TABLE_NAME = "ПРЕДПОСЕВНАЯ ОБРАБОТКА ПОЧВЫ С ПОСЕВОМ СЕЛЬСКОХОЗЯЙСТВЕННЫХ КУЛЬТУР: ПШЕНИЦЫ, РЖИ, ЯЧМЕНЯ, ОВСА, ГОРОХА, ЛЮПИНА, ВИКИ, ВИКООВСЯНОЙ СМЕСИ, ЛЬНА";
+
+    public SeventhTableFuelSearchingArgumentsProvider() {
+        super(TABLE_NAME);
+    }
 
     @Override
-    protected Stream<FuelSearchingArguments> createFuelSearchingArguments() {
+    protected Stream<FuelSearchingArguments> createFuelSearchingArguments(
+            final Supplier<FuelSpecificationBuilder> specificationBuilderSupplier
+    ) {
         return Stream.of(
                 FuelSearchingArguments.builder()
-                        .specification(FuelSpecification.builder()
-                                .tableName("ПРЕДПОСЕВНАЯ ОБРАБОТКА ПОЧВЫ С ПОСЕВОМ СЕЛЬСКОХОЗЯЙСТВЕННЫХ КУЛЬТУР: ПШЕНИЦЫ, РЖИ, ЯЧМЕНЯ, ОВСА, ГОРОХА, ЛЮПИНА, ВИКИ, ВИКООВСЯНОЙ СМЕСИ, ЛЬНА")
+                        .specification(specificationBuilderSupplier.get()
                                 .tractor("Кировец К-744 Р3")
                                 .machinery("Horsch Serto 12 SC")
                                 .workingWidth("12")
@@ -23,8 +30,7 @@ public final class SeventhTableFuelSearchingArgumentsProvider extends TableFuelS
                         .expected(new Fuel(28.9, 8.2))
                         .build(),
                 FuelSearchingArguments.builder()
-                        .specification(FuelSpecification.builder()
-                                .tableName("ПРЕДПОСЕВНАЯ ОБРАБОТКА ПОЧВЫ С ПОСЕВОМ СЕЛЬСКОХОЗЯЙСТВЕННЫХ КУЛЬТУР: ПШЕНИЦЫ, РЖИ, ЯЧМЕНЯ, ОВСА, ГОРОХА, ЛЮПИНА, ВИКИ, ВИКООВСЯНОЙ СМЕСИ, ЛЬНА")
+                        .specification(specificationBuilderSupplier.get()
                                 .tractor("Беларус 3022")
                                 .machinery("Horsch Pronto 6 DS")
                                 .workingWidth("6")
@@ -34,8 +40,7 @@ public final class SeventhTableFuelSearchingArgumentsProvider extends TableFuelS
                         .expected(new Fuel(18.4, 9.3))
                         .build(),
                 FuelSearchingArguments.builder()
-                        .specification(FuelSpecification.builder()
-                                .tableName("ПРЕДПОСЕВНАЯ ОБРАБОТКА ПОЧВЫ С ПОСЕВОМ СЕЛЬСКОХОЗЯЙСТВЕННЫХ КУЛЬТУР: ПШЕНИЦЫ, РЖИ, ЯЧМЕНЯ, ОВСА, ГОРОХА, ЛЮПИНА, ВИКИ, ВИКООВСЯНОЙ СМЕСИ, ЛЬНА")
+                        .specification(specificationBuilderSupplier.get()
                                 .tractor("Беларус 1221")
                                 .machinery("Horsch Sprinter 4 ST")
                                 .workingWidth("4")
@@ -46,8 +51,7 @@ public final class SeventhTableFuelSearchingArgumentsProvider extends TableFuelS
                         .build(),
                 //not existing tractor
                 FuelSearchingArguments.builder()
-                        .specification(FuelSpecification.builder()
-                                .tableName("ПРЕДПОСЕВНАЯ ОБРАБОТКА ПОЧВЫ С ПОСЕВОМ СЕЛЬСКОХОЗЯЙСТВЕННЫХ КУЛЬТУР: ПШЕНИЦЫ, РЖИ, ЯЧМЕНЯ, ОВСА, ГОРОХА, ЛЮПИНА, ВИКИ, ВИКООВСЯНОЙ СМЕСИ, ЛЬНА")
+                        .specification(specificationBuilderSupplier.get()
                                 .tractor("not existing")
                                 .machinery("Horsch Sprinter 4 ST")
                                 .workingWidth("4")
@@ -57,8 +61,7 @@ public final class SeventhTableFuelSearchingArgumentsProvider extends TableFuelS
                         .build(),
                 //not existing machinery
                 FuelSearchingArguments.builder()
-                        .specification(FuelSpecification.builder()
-                                .tableName("ПРЕДПОСЕВНАЯ ОБРАБОТКА ПОЧВЫ С ПОСЕВОМ СЕЛЬСКОХОЗЯЙСТВЕННЫХ КУЛЬТУР: ПШЕНИЦЫ, РЖИ, ЯЧМЕНЯ, ОВСА, ГОРОХА, ЛЮПИНА, ВИКИ, ВИКООВСЯНОЙ СМЕСИ, ЛЬНА")
+                        .specification(specificationBuilderSupplier.get()
                                 .tractor("Беларус 1221")
                                 .machinery("not existing")
                                 .workingWidth("4")
@@ -68,8 +71,7 @@ public final class SeventhTableFuelSearchingArgumentsProvider extends TableFuelS
                         .build(),
                 //not existing working width
                 FuelSearchingArguments.builder()
-                        .specification(FuelSpecification.builder()
-                                .tableName("ПРЕДПОСЕВНАЯ ОБРАБОТКА ПОЧВЫ С ПОСЕВОМ СЕЛЬСКОХОЗЯЙСТВЕННЫХ КУЛЬТУР: ПШЕНИЦЫ, РЖИ, ЯЧМЕНЯ, ОВСА, ГОРОХА, ЛЮПИНА, ВИКИ, ВИКООВСЯНОЙ СМЕСИ, ЛЬНА")
+                        .specification(specificationBuilderSupplier.get()
                                 .tractor("Беларус 1221")
                                 .machinery("Horsch Sprinter 4 ST")
                                 .workingWidth("not existing")
@@ -79,8 +81,7 @@ public final class SeventhTableFuelSearchingArgumentsProvider extends TableFuelS
                         .build(),
                 //not existing routing length
                 FuelSearchingArguments.builder()
-                        .specification(FuelSpecification.builder()
-                                .tableName("ПРЕДПОСЕВНАЯ ОБРАБОТКА ПОЧВЫ С ПОСЕВОМ СЕЛЬСКОХОЗЯЙСТВЕННЫХ КУЛЬТУР: ПШЕНИЦЫ, РЖИ, ЯЧМЕНЯ, ОВСА, ГОРОХА, ЛЮПИНА, ВИКИ, ВИКООВСЯНОЙ СМЕСИ, ЛЬНА")
+                        .specification(specificationBuilderSupplier.get()
                                 .tractor("Беларус 1221")
                                 .machinery("Horsch Sprinter 4 ST")
                                 .workingWidth("4")
@@ -90,8 +91,7 @@ public final class SeventhTableFuelSearchingArgumentsProvider extends TableFuelS
                         .build(),
                 //not existing sowing norm
                 FuelSearchingArguments.builder()
-                        .specification(FuelSpecification.builder()
-                                .tableName("ПРЕДПОСЕВНАЯ ОБРАБОТКА ПОЧВЫ С ПОСЕВОМ СЕЛЬСКОХОЗЯЙСТВЕННЫХ КУЛЬТУР: ПШЕНИЦЫ, РЖИ, ЯЧМЕНЯ, ОВСА, ГОРОХА, ЛЮПИНА, ВИКИ, ВИКООВСЯНОЙ СМЕСИ, ЛЬНА")
+                        .specification(specificationBuilderSupplier.get()
                                 .tractor("Беларус 1221")
                                 .machinery("Horsch Sprinter 4 ST")
                                 .workingWidth("4")
