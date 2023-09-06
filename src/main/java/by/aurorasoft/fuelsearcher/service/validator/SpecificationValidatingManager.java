6ded5555a76ac9a97da2eec485e1a30809ef8832
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-06 16:50:08 +0300

    refactor SpecificationValidatingManager

--- src/main/java/by/aurorasoft/fuelsearcher/service/validator/SpecificationValidatingManager.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/validator/SpecificationValidatingManager.java
@@ -1,20 +1,23 @@
 package by.aurorasoft.fuelsearcher.service.validator;
 
 import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.TableNameExtractor;
 import by.aurorasoft.fuelsearcher.service.dictionary.SpecificationValidatorDictionary;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static by.aurorasoft.fuelsearcher.service.validator.SpecificationValidatingResult.createNotValidValidatingResult;
 
 @Service
 @RequiredArgsConstructor
 public final class SpecificationValidatingManager {
     private final SpecificationValidatorDictionary validatorDictionary;
+    private final TableNameExtractor tableNameExtractor;
 
-    public boolean isValid(final FuelSpecification specification) {
-//        return specification.findTableName()
-//                .flatMap(this.validatorDictionary::find)
-//                .map(validator -> validator.validate(specification))
-//                .orElse(false);
-        return true;
+    public SpecificationValidatingResult validate(final FuelSpecification specification) {
+        return this.tableNameExtractor.findProperty(specification)
+                .flatMap(this.validatorDictionary::find)
+                .map(validator -> validator.validate(specification))
+                .orElseGet(() -> createNotValidValidatingResult(this.tableNameExtractor));
     }
 }
