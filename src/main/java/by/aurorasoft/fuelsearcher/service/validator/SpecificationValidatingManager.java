9df41f937a538510c0978760437a4f9488a7ca27
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-06 12:23:32 +0300

    refactor SpecificationValidatingManager

--- src/main/java/by/aurorasoft/fuelsearcher/service/validator/SpecificationValidatingManager.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/validator/SpecificationValidatingManager.java
@@ -1,8 +1,19 @@
 package by.aurorasoft.fuelsearcher.service.validator;
 
+import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification;
+import by.aurorasoft.fuelsearcher.service.dictionary.SpecificationValidatorDictionary;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public final class SpecificationValidatingManager {
+    private final SpecificationValidatorDictionary validatorDictionary;
 
+    public boolean isValid(final FuelSpecification specification) {
+        return specification.findTableName()
+                .flatMap(this.validatorDictionary::find)
+                .map(validator -> validator.isValid(specification))
+                .orElse(false);
+    }
 }
