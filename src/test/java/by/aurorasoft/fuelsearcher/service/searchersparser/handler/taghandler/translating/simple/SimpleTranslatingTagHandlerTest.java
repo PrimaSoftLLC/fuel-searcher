f4545b0e1e9e26ce9df3db4729e804297e9e22d4
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-07 17:24:18 +0300

    add SimpleTranslatingTagHandlerTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/service/searchersparser/handler/taghandler/translating/simple/SimpleTranslatingTagHandlerTest.java
@@ -0,0 +1,67 @@
+package by.aurorasoft.fuelsearcher.service.searchersparser.handler.taghandler.translating.simple;
+
+import by.aurorasoft.fuelsearcher.service.dictionary.Translatable;
+import by.aurorasoft.fuelsearcher.service.searchersparser.handler.SearchersParsingContext;
+import lombok.RequiredArgsConstructor;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class SimpleTranslatingTagHandlerTest {
+
+    @Test
+    public void aliasesShouldBeFound() {
+        final String givenLastContent = "key";
+        final SearchersParsingContext givenContext = createContext(givenLastContent);
+        final TestSimpleTranslatingTagHandler givenTagHandler = new TestSimpleTranslatingTagHandler();
+
+        final Stream<String> actual = givenTagHandler.findAliases(givenContext);
+        final List<String> actualAsList = actual.toList();
+        final List<String> expectedAsList = List.of(givenLastContent);
+        assertEquals(expectedAsList, actualAsList);
+    }
+
+    @SuppressWarnings("SameParameterValue")
+    private static SearchersParsingContext createContext(final String lastContent) {
+        final SearchersParsingContext context = mock(SearchersParsingContext.class);
+        when(context.getLastContent()).thenReturn(lastContent);
+        return context;
+    }
+
+    @RequiredArgsConstructor
+    private static final class TestValue implements Translatable {
+        private final String alias;
+
+        @Override
+        public String findAlias() {
+            return this.alias;
+        }
+    }
+
+    private static final class TestSimpleTranslatingTagHandler extends SimpleTranslatingTagHandler<TestValue> {
+
+        public TestSimpleTranslatingTagHandler() {
+            super(null, null, null);
+        }
+
+        @Override
+        public void handleStartTag(final SearchersParsingContext context) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        protected void accumulateTranslatedValue(final SearchersParsingContext context, final TestValue value) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        protected void accumulateAdditionalValues(final SearchersParsingContext context) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
