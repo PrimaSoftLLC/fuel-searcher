2c4fc9bde48f996a208431ac9123f74c4e5ed9c0
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-07 16:38:50 +0300

    refactor SubTableTitleTemplateTagHandler

--- src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/handler/taghandler/translating/SubTableTitleTemplateTagHandler.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/handler/taghandler/translating/SubTableTitleTemplateTagHandler.java
@@ -1,10 +1,9 @@
-package by.aurorasoft.fuelsearcher.service.searchersparser.handler.taghandler.translating.subtabletitle;
+package by.aurorasoft.fuelsearcher.service.searchersparser.handler.taghandler.translating;
 
-import by.aurorasoft.fuelsearcher.service.dictionary.SpecificationPropertyExtractorDictionary;
 import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.SpecificationPropertyExtractor;
+import by.aurorasoft.fuelsearcher.service.dictionary.SpecificationPropertyExtractorDictionary;
 import by.aurorasoft.fuelsearcher.service.searchersparser.handler.SearchersParsingContext;
-import by.aurorasoft.fuelsearcher.service.searchersparser.handler.taghandler.translating.subtabletitle.exception.NoSuchSpecificationPropertyExtractorException;
-import by.aurorasoft.fuelsearcher.service.searchersparser.handler.taghandler.translating.TranslatingTagHandler;
+import by.aurorasoft.fuelsearcher.service.searchersparser.handler.taghandler.translating.exception.NoSuchKeyException;
 import org.springframework.stereotype.Component;
 
 import java.util.regex.Pattern;
@@ -54,5 +53,27 @@ public final class SubTableTitleTemplateTagHandler extends TranslatingTagHandler
     private static String findSubTableTitleTemplateWithStringFillers(final SearchersParsingContext context) {
         final String initialSubTableTitleTemplate = context.getLastContent();
         return initialSubTableTitleTemplate.replaceAll(PROPERTY_PLACE_REGEX, STRING_FILLER);
+    }
+
+    private static final class NoSuchSpecificationPropertyExtractorException extends NoSuchKeyException {
+
+        @SuppressWarnings("unused")
+        public NoSuchSpecificationPropertyExtractorException() {
+
+        }
+
+        public NoSuchSpecificationPropertyExtractorException(final String key) {
+            super(key);
+        }
+
+        @SuppressWarnings("unused")
+        public NoSuchSpecificationPropertyExtractorException(final Exception cause) {
+            super(cause);
+        }
+
+        @SuppressWarnings("unused")
+        public NoSuchSpecificationPropertyExtractorException(final String key, final Exception cause) {
+            super(key, cause);
+        }
     }
 }
