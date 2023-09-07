b71be8367e3a892d87f49d7b45bec373415c908c
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-07 10:33:37 +0300

    refactor FuelControllerExceptionHandler

--- src/main/java/by/aurorasoft/fuelsearcher/controller/exceptionhandler/FuelControllerExceptionHandler.java
+++ src/main/java/by/aurorasoft/fuelsearcher/controller/exceptionhandler/FuelControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package by.aurorasoft.fuelsearcher.controller.exceptionhandler;
 
 import by.aurorasoft.fuelsearcher.controller.exception.NoSuchFuelException;
 import by.aurorasoft.fuelsearcher.controller.exception.NotValidSpecificationException;
+import by.aurorasoft.fuelsearcher.service.validator.SpecificationValidatingResult;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Value;
 import org.springframework.http.HttpStatus;
@@ -10,35 +11,51 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static com.fasterxml.jackson.annotation.JsonFormat.Shape.STRING;
+import static java.lang.String.join;
 import static java.time.LocalDateTime.now;
 import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
 public final class FuelControllerExceptionHandler {
+    private static final String TEMPLATE_MESSAGE_NOT_VALID_SPECIFICATION = "Specification should contain properties: %s";
+    private static final String DELIMITER_SPECIFICATION_PROPERTIES = ",";
 
     @ExceptionHandler
     public ResponseEntity<RestErrorResponse> handleException(final NoSuchFuelException exception) {
-        return createResponseEntity(exception, NOT_FOUND);
+        final String message = exception.getMessage();
+        return createResponseEntity(message, NOT_FOUND);
     }
 
     @ExceptionHandler
     public ResponseEntity<RestErrorResponse> handleException(final NotValidSpecificationException exception) {
-        return createResponseEntity(exception, NOT_ACCEPTABLE);
+        final String message = findMessage(exception);
+        return createResponseEntity(message, NOT_ACCEPTABLE);
     }
 
-    private static ResponseEntity<RestErrorResponse> createResponseEntity(final Exception exception,
+    private static ResponseEntity<RestErrorResponse> createResponseEntity(final String message,
                                                                           final HttpStatus httpStatus) {
-        final RestErrorResponse errorResponse = createErrorResponse(exception, httpStatus);
+        final RestErrorResponse errorResponse = createErrorResponse(message, httpStatus);
         return new ResponseEntity<>(errorResponse, httpStatus);
     }
 
-    private static RestErrorResponse createErrorResponse(final Exception exception, final HttpStatus httpStatus) {
-        final String message = exception.getMessage();
+    private static RestErrorResponse createErrorResponse(final String message, final HttpStatus httpStatus) {
         final LocalDateTime currentDateTime = now();
         return new RestErrorResponse(httpStatus, message, currentDateTime);
+    }
+
+    private static String findMessage(final NotValidSpecificationException exception) {
+        final SpecificationValidatingResult validatingResult = exception.getValidatingResult();
+        final String seperatedFailedPropertyNames = findSeperatedFailedPropertyNames(validatingResult);
+        return TEMPLATE_MESSAGE_NOT_VALID_SPECIFICATION.formatted(seperatedFailedPropertyNames);
+    }
+
+    private static String findSeperatedFailedPropertyNames(final SpecificationValidatingResult validatingResult) {
+        final List<String> failedPropertyNames = validatingResult.findFailedPropertyNames();
+        return join(DELIMITER_SPECIFICATION_PROPERTIES, failedPropertyNames);
     }
 
     @Value
