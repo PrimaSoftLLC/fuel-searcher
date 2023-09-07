a309174e09649282880375c992464e3bb83a4c6c
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-07 10:18:11 +0300

    refactor NotValidSpecificationException

--- src/main/java/by/aurorasoft/fuelsearcher/controller/exception/NotValidSpecificationException.java
+++ src/main/java/by/aurorasoft/fuelsearcher/controller/exception/NotValidSpecificationException.java
@@ -1,21 +1,30 @@
 package by.aurorasoft.fuelsearcher.controller.exception;
 
+import by.aurorasoft.fuelsearcher.service.validator.SpecificationValidatingResult;
+
 public final class NotValidSpecificationException extends RuntimeException {
+    private final SpecificationValidatingResult validatingResult;
 
-    public NotValidSpecificationException() {
-
+    public NotValidSpecificationException(final SpecificationValidatingResult validatingResult) {
+        this.validatingResult = validatingResult;
     }
 
-    public NotValidSpecificationException(final String description) {
+    public NotValidSpecificationException(final String description,
+                                          final SpecificationValidatingResult validatingResult) {
         super(description);
+        this.validatingResult = validatingResult;
     }
 
-    public NotValidSpecificationException(final Exception cause) {
+    public NotValidSpecificationException(final Exception cause, final SpecificationValidatingResult validatingResult) {
         super(cause);
+        this.validatingResult = validatingResult;
     }
 
-    public NotValidSpecificationException(final String description, final Exception cause) {
+    public NotValidSpecificationException(final String description,
+                                          final Exception cause,
+                                          final SpecificationValidatingResult validatingResult) {
         super(description, cause);
+        this.validatingResult = validatingResult;
     }
 
 }
