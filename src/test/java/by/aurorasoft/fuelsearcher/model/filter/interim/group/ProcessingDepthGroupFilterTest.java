11662ccfac67a6262afa28e6c535942539e9e6df
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-08 16:03:52 +0300

    add ProcessingDepthGroupFilterTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/ProcessingDepthGroupFilterTest.java
@@ -0,0 +1,21 @@
+package by.aurorasoft.fuelsearcher.model.filter.interim.group;
+
+import org.junit.Test;
+
+import static java.lang.Integer.MIN_VALUE;
+import static org.junit.Assert.assertEquals;
+
+public final class ProcessingDepthGroupFilterTest {
+
+    @Test
+    public void groupValueRegexShouldBeFound() {
+        final ProcessingDepthGroupFilter givenFilter = new ProcessingDepthGroupFilter(
+                null, MIN_VALUE
+        );
+
+        final String actual = givenFilter.findGroupValueRegex();
+        final String expected = "Глубина обработки \\d+...\\d+ см";
+        assertEquals(expected, actual);
+    }
+
+}
