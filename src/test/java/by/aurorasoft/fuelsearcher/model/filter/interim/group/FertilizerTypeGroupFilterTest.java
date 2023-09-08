be7f800ceeff88ea42e140ea40f75de55a7f5dc0
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-08 15:58:21 +0300

    refactor FertilizerTypeGroupFilter and add FertilizerTypeGroupFilterTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/FertilizerTypeGroupFilterTest.java
@@ -0,0 +1,19 @@
+package by.aurorasoft.fuelsearcher.model.filter.interim.group;
+
+import org.junit.Test;
+
+import static java.lang.Integer.MIN_VALUE;
+import static org.junit.Assert.assertEquals;
+
+public final class FertilizerTypeGroupFilterTest {
+
+    @Test
+    public void groupValueRegexShouldBeFound() {
+        final FertilizerTypeGroupFilter givenFilter = new FertilizerTypeGroupFilter(null, MIN_VALUE);
+
+        final String actual = givenFilter.findGroupValueRegex();
+        final String expected = "(Гранулированные|Кристаллические|Пылевидные) удобрения";
+        assertEquals(expected, actual);
+    }
+
+}
