c5fd789377935e7fe895f19e882dde48abaf2842
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-08 17:29:42 +0300

    add TransportDistanceFinalFilterFactoryTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/conclusive/TransportDistanceFinalFilterFactoryTest.java
@@ -0,0 +1,28 @@
+package by.aurorasoft.fuelsearcher.model.filter.factory.conclusive;
+
+import by.aurorasoft.fuelsearcher.model.filter.conclusive.TransportDistanceFinalFilter;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.TransportDistanceExtractor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+
+public final class TransportDistanceFinalFilterFactoryTest {
+    private final TransportDistanceFinalFilterFactory factory = new TransportDistanceFinalFilterFactory(
+            null
+    );
+
+    @Test
+    public void filterShouldBeCreated() {
+        final TransportDistanceExtractor givenTransportDistanceExtractor = mock(TransportDistanceExtractor.class);
+        final int givenFiltrationCellIndex = 1;
+
+        final TransportDistanceFinalFilter actual = this.factory.create(
+                givenTransportDistanceExtractor, givenFiltrationCellIndex
+        );
+        assertSame(givenTransportDistanceExtractor, actual.getFiltrationValueExtractor());
+        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
+    }
+
+}
