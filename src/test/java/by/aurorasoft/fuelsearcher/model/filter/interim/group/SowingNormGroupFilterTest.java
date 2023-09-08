00b3b225cff7ffd89c8a89d670d18893514ccf50
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-08 16:16:20 +0300

    add SowingNormGroupFilterTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/SowingNormGroupFilterTest.java
@@ -0,0 +1,19 @@
+package by.aurorasoft.fuelsearcher.model.filter.interim.group;
+
+import org.junit.Test;
+
+import static java.lang.Integer.MIN_VALUE;
+import static org.junit.Assert.assertEquals;
+
+public final class SowingNormGroupFilterTest {
+
+    @Test
+    public void groupValueRegexShouldBeFound() {
+        final SowingNormGroupFilter givenFilter = new SowingNormGroupFilter(null, MIN_VALUE);
+
+        final String actual = givenFilter.findGroupValueRegex();
+        final String expected = "Норма высева (семян )?\\d+(-\\d+)? кг/га";
+        assertEquals(expected, actual);
+    }
+
+}
