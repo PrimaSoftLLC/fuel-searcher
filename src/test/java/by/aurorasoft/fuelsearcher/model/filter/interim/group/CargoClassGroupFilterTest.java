f85ced3cf943c051559b8ba6df6f730bb37f3d11
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-08 15:41:40 +0300

    add CargoClassGroupFilterTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/CargoClassGroupFilterTest.java
@@ -0,0 +1,19 @@
+package by.aurorasoft.fuelsearcher.model.filter.interim.group;
+
+import org.junit.Test;
+
+import static java.lang.Integer.MIN_VALUE;
+import static org.junit.Assert.assertEquals;
+
+public final class CargoClassGroupFilterTest {
+
+    @Test
+    public void groupValueRegexShouldBeFound() {
+        final CargoClassGroupFilter givenFilter = new CargoClassGroupFilter(null, MIN_VALUE);
+
+        final String actual = givenFilter.findGroupValueRegex();
+        final String expected = "Грузы (I|II|III|IV) класса";
+        assertEquals(expected, actual);
+    }
+
+}
---
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/GroupFilterTest.java
@@ -0,0 +1,12 @@
+package by.aurorasoft.fuelsearcher.model.filter.interim.group;
+
+import org.junit.Test;
+
+public final class GroupFilterTest {
+
+    @Test
+    public void a() {
+        throw new RuntimeException();
+    }
+
+}
