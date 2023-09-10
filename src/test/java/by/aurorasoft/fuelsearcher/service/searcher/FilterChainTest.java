278faafcd1dc772d097684f3ffdd88ba9dbc9243
Vlad Zuev <youtube.vladzuev.00@mail.ru>
2023-09-11 00:55:58 +0300

    refactor

---
+++ src/test/java/by/aurorasoft/fuelsearcher/service/searcher/FilterChainTest.java
@@ -0,0 +1,157 @@
+package by.aurorasoft.fuelsearcher.service.searcher;
+
+import by.aurorasoft.fuelsearcher.model.filter.conclusive.FinalFilter;
+import by.aurorasoft.fuelsearcher.model.filter.interim.InterimFilter;
+import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification;
+import by.aurorasoft.fuelsearcher.service.searcher.FilterChain.FilterChainBuilder;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public final class FilterChainTest {
+    private static final String FIELD_NAME_INTERIM_FILTERS = "interimFilters";
+    private static final String FIELD_NAME_FINAL_FILTER = "finalFilter";
+
+    @Test
+    public void builderShouldBeCreated() {
+        final FilterChainBuilder actual = FilterChain.builder();
+        assertNotNull(actual);
+    }
+
+    @Test
+    public void rowsShouldBeFilteredToOneRow()
+            throws Exception {
+        final InterimFilter firstGivenInterimFilter = mock(InterimFilter.class);
+        final InterimFilter secondGivenInterimFilter = mock(InterimFilter.class);
+        final List<InterimFilter> givenInterimFilters = List.of(firstGivenInterimFilter, secondGivenInterimFilter);
+        final FinalFilter givenFinalFilter = mock(FinalFilter.class);
+        final FilterChain givenFilterChain = createChain(givenInterimFilters, givenFinalFilter);
+
+        final FuelSpecification givenSpecification = mock(FuelSpecification.class);
+
+        final XWPFTableRow firstGivenRow = mock(XWPFTableRow.class);
+        final XWPFTableRow secondGivenRow = mock(XWPFTableRow.class);
+        final XWPFTableRow thirdGivenRow = mock(XWPFTableRow.class);
+        final List<XWPFTableRow> givenInitialRows = List.of(firstGivenRow, secondGivenRow, thirdGivenRow);
+
+        final List<XWPFTableRow> givenFirstInterimFilterResult = List.of(firstGivenRow, secondGivenRow);
+        when(firstGivenInterimFilter.filter(same(givenInitialRows), same(givenSpecification)))
+                .thenReturn(givenFirstInterimFilterResult);
+
+        final List<XWPFTableRow> givenSecondInterimFilterResult = List.of(firstGivenRow, secondGivenRow);
+        when(secondGivenInterimFilter.filter(same(givenFirstInterimFilterResult), same(givenSpecification)))
+                .thenReturn(givenSecondInterimFilterResult);
+
+        when(givenFinalFilter.filter(same(givenSecondInterimFilterResult), same(givenSpecification)))
+                .thenReturn(Optional.of(firstGivenRow));
+
+        final Optional<XWPFTableRow> optionalActual = givenFilterChain.filter(givenInitialRows, givenSpecification);
+        assertTrue(optionalActual.isPresent());
+        final XWPFTableRow actual = optionalActual.get();
+        assertSame(firstGivenRow, actual);
+    }
+
+    @Test
+    public void allRowsShouldBeFiltered()
+            throws Exception {
+        final InterimFilter firstGivenInterimFilter = mock(InterimFilter.class);
+        final InterimFilter secondGivenInterimFilter = mock(InterimFilter.class);
+        final List<InterimFilter> givenInterimFilters = List.of(firstGivenInterimFilter, secondGivenInterimFilter);
+        final FinalFilter givenFinalFilter = mock(FinalFilter.class);
+        final FilterChain givenFilterChain = createChain(givenInterimFilters, givenFinalFilter);
+
+        final FuelSpecification givenSpecification = mock(FuelSpecification.class);
+
+        final XWPFTableRow firstGivenRow = mock(XWPFTableRow.class);
+        final XWPFTableRow secondGivenRow = mock(XWPFTableRow.class);
+        final XWPFTableRow thirdGivenRow = mock(XWPFTableRow.class);
+        final List<XWPFTableRow> givenInitialRows = List.of(firstGivenRow, secondGivenRow, thirdGivenRow);
+
+        final List<XWPFTableRow> givenFirstInterimFilterResult = List.of(firstGivenRow, secondGivenRow);
+        when(firstGivenInterimFilter.filter(same(givenInitialRows), same(givenSpecification)))
+                .thenReturn(givenFirstInterimFilterResult);
+
+        final List<XWPFTableRow> givenSecondInterimFilterResult = List.of(firstGivenRow, secondGivenRow);
+        when(secondGivenInterimFilter.filter(same(givenFirstInterimFilterResult), same(givenSpecification)))
+                .thenReturn(givenSecondInterimFilterResult);
+
+        when(givenFinalFilter.filter(same(givenSecondInterimFilterResult), same(givenSpecification)))
+                .thenReturn(empty());
+
+        final Optional<XWPFTableRow> optionalActual = givenFilterChain.filter(givenInitialRows, givenSpecification);
+        assertTrue(optionalActual.isEmpty());
+    }
+
+    @Test
+    public void interimFilterShouldBeAccumulatedByBuilder()
+            throws Exception {
+        final FilterChainBuilder givenBuilder = FilterChain.builder();
+
+        final InterimFilter firstGivenInterimFilter = mock(InterimFilter.class);
+        final InterimFilter secondGivenInterimFilter = mock(InterimFilter.class);
+
+        givenBuilder.interimFilter(firstGivenInterimFilter);
+        givenBuilder.interimFilter(secondGivenInterimFilter);
+
+        final List<InterimFilter> actual = findInterimFilters(givenBuilder);
+        final List<InterimFilter> expected = List.of(firstGivenInterimFilter, secondGivenInterimFilter);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void finalFilterShouldBeAccumulatedToBuilder() {
+        final FilterChainBuilder givenBuilder = FilterChain.builder();
+
+        final FinalFilter givenFinalFilter = mock(FinalFilter.class);
+
+        throw new RuntimeException();
+    }
+
+    private static FilterChain createChain(final List<InterimFilter> interimFilters, final FinalFilter finalFilter)
+            throws Exception {
+        final Constructor<FilterChain> constructor = FilterChain.class.getDeclaredConstructor(
+                List.class, FinalFilter.class
+        );
+        constructor.setAccessible(true);
+        try {
+            return constructor.newInstance(interimFilters, finalFilter);
+        } finally {
+            constructor.setAccessible(false);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<InterimFilter> findInterimFilters(final FilterChainBuilder builder)
+            throws Exception {
+        return findBuilderProperty(builder, FIELD_NAME_INTERIM_FILTERS, List.class);
+    }
+
+    private static FinalFilter findFinalFilter(final FilterChainBuilder builder)
+            throws Exception {
+        return findBuilderProperty(builder, FIELD_NAME_FINAL_FILTER, FinalFilter.class);
+    }
+
+    private static <T> T findBuilderProperty(final FilterChainBuilder builder,
+                                             final String fieldName,
+                                             final Class<T> propertyType)
+            throws Exception {
+        final Field field = FilterChainBuilder.class.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        try {
+            final Object property = field.get(builder);
+            return propertyType.cast(property);
+        } finally {
+            field.setAccessible(false);
+        }
+    }
+}
