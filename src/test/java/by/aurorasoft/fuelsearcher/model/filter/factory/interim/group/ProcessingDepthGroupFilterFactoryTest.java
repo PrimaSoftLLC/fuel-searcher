b0579d52c5914c1077ba3d3913a2f76ea36a46c5
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-11 10:25:57 +0300

    add ProcessingDepthGroupFilterFactoryTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/interim/group/ProcessingDepthGroupFilterFactoryTest.java
@@ -0,0 +1,29 @@
+package by.aurorasoft.fuelsearcher.model.filter.factory.interim.group;
+
+import by.aurorasoft.fuelsearcher.model.filter.interim.group.ProcessingDepthGroupFilter;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.ProcessingDepthExtractor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+
+public final class ProcessingDepthGroupFilterFactoryTest {
+    private final ProcessingDepthGroupFilterFactory factory = new ProcessingDepthGroupFilterFactory(
+            null
+    );
+
+    @Test
+    public void filterShouldBeCreated() {
+        final ProcessingDepthExtractor givenProcessingDepthExtractor = mock(ProcessingDepthExtractor.class);
+        final int givenFiltrationCellIndex = 3;
+
+        final ProcessingDepthGroupFilter actual = this.factory.create(
+                givenProcessingDepthExtractor, givenFiltrationCellIndex
+        );
+
+        assertSame(givenProcessingDepthExtractor, actual.getFiltrationValueExtractor());
+        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
+    }
+
+}
