fc175fed426a58a2f9cebb69fbc5538fc4fd0c48
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-11 11:20:50 +0300

    add CorpusCountUnitFilterFactoryTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/interim/unit/CorpusCountUnitFilterFactoryTest.java
@@ -0,0 +1,24 @@
+package by.aurorasoft.fuelsearcher.model.filter.factory.interim.unit;
+
+import by.aurorasoft.fuelsearcher.model.filter.interim.unit.CorpusCountUnitFilter;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.CorpusCountExtractor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+
+public final class CorpusCountUnitFilterFactoryTest {
+    private final CorpusCountUnitFilterFactory factory = new CorpusCountUnitFilterFactory(null);
+
+    @Test
+    public void filterShouldBeCreated() {
+        final CorpusCountExtractor givenCorpusCountExtractor = mock(CorpusCountExtractor.class);
+        final int givenFiltrationCellIndex = 5;
+
+        final CorpusCountUnitFilter actual = this.factory.create(givenCorpusCountExtractor, givenFiltrationCellIndex);
+
+        assertSame(givenCorpusCountExtractor, actual.getFiltrationValueExtractor());
+        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
+    }
+}
