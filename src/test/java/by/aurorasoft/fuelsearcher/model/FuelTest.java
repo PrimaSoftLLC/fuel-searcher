aa3db7767e7a5f20181bd32dbeb94a9b1ea883de
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-11 15:12:44 +0300

    refactor FuelTest

--- src/test/java/by/aurorasoft/fuelsearcher/model/FuelTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/model/FuelTest.java
@@ -1,16 +1,21 @@
 package by.aurorasoft.fuelsearcher.model;
 
+import by.aurorasoft.fuelsearcher.base.AbstractContextTest;
 import by.aurorasoft.fuelsearcher.util.XWPFTableCellUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.mockito.MockedStatic;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static by.aurorasoft.fuelsearcher.util.XWPFTableCellUtil.isNotDefinedDouble;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mockStatic;
 
-public final class FuelTest {
+public final class FuelTest extends AbstractContextTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     @SuppressWarnings("ResultOfMethodCallIgnored")
@@ -74,5 +79,25 @@ public final class FuelTest {
             final boolean actual = givenFuel.isDefinedFuel();
             assertFalse(actual);
         }
+    }
+
+    @Test
+    public void fuelShouldBeConvertedToJson()
+            throws Exception {
+        final Fuel givenFuel = new Fuel(5.5, 6.6);
+
+        final String actual = this.objectMapper.writeValueAsString(givenFuel);
+        final String expected = "{\"generationNorm\":5.5,\"consumption\":6.6}";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void jsonShouldBeConvertedToFuel()
+            throws Exception {
+        final String givenJson = "{\"generationNorm\":5.5,\"consumption\":6.6}";
+
+        final Fuel actual = this.objectMapper.readValue(givenJson, Fuel.class);
+        final Fuel expected = new Fuel(5.5, 6.6);
+        assertEquals(expected, actual);
     }
 }
