905e93ef26324911fcdcd176ae6ec22d9102e8a1
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-11 11:38:41 +0300

    add WorkingWidthUnitFilterFactoryTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/interim/unit/WorkingWidthUnitFilterFactoryTest.java
@@ -0,0 +1,24 @@
+package by.aurorasoft.fuelsearcher.model.filter.factory.interim.unit;
+
+import by.aurorasoft.fuelsearcher.model.filter.interim.unit.WorkingWidthUnitFilter;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.WorkingWidthExtractor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+
+public final class WorkingWidthUnitFilterFactoryTest {
+    private final WorkingWidthUnitFilterFactory factory = new WorkingWidthUnitFilterFactory(null);
+
+    @Test
+    public void filterShouldBeCreated() {
+        final WorkingWidthExtractor givenWorkingWidthExtractor = mock(WorkingWidthExtractor.class);
+        final int givenFiltrationCellIndex = 5;
+
+        final WorkingWidthUnitFilter actual = this.factory.create(givenWorkingWidthExtractor, givenFiltrationCellIndex);
+
+        assertSame(givenWorkingWidthExtractor, actual.getFiltrationValueExtractor());
+        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
+    }
+}
