85162aecf70c24eaaa079df5fdc827f8bcf61481
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-11 16:19:57 +0300

    refactor NoSuchFuelException

--- src/main/java/by/aurorasoft/fuelsearcher/controller/exception/NoSuchFuelException.java
+++ src/main/java/by/aurorasoft/fuelsearcher/controller/exception/NoSuchFuelException.java
@@ -2,6 +2,7 @@ package by.aurorasoft.fuelsearcher.controller.exception;
 
 public final class NoSuchFuelException extends RuntimeException {
 
+    @SuppressWarnings("unused")
     public NoSuchFuelException() {
 
     }
@@ -10,10 +11,12 @@ public final class NoSuchFuelException extends RuntimeException {
         super(description);
     }
 
+    @SuppressWarnings("unused")
     public NoSuchFuelException(final Exception cause) {
         super(cause);
     }
 
+    @SuppressWarnings("unused")
     public NoSuchFuelException(final String description, final Exception cause) {
         super(description, cause);
     }
