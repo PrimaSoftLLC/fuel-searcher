3a1bc7455e9a297e980ff38516d5bf9d2ab4dfa9
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-11 11:29:46 +0300

    add RowWidthUnitFilterFactoryTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/interim/unit/RowWidthUnitFilterFactoryTest.java
@@ -0,0 +1,24 @@
+package by.aurorasoft.fuelsearcher.model.filter.factory.interim.unit;
+
+import by.aurorasoft.fuelsearcher.model.filter.interim.unit.RowWidthUnitFilter;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.RowWidthExtractor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+
+public final class RowWidthUnitFilterFactoryTest {
+    private final RowWidthUnitFilterFactory factory = new RowWidthUnitFilterFactory(null);
+
+    @Test
+    public void filterShouldBeCreated() {
+        final RowWidthExtractor givenRowWidthExtractor = mock(RowWidthExtractor.class);
+        final int givenFiltrationCellIndex = 2;
+
+        final RowWidthUnitFilter actual = this.factory.create(givenRowWidthExtractor, givenFiltrationCellIndex);
+
+        assertSame(givenRowWidthExtractor, actual.getFiltrationValueExtractor());
+        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
+    }
+}
