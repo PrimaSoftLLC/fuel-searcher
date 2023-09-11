15c7634a22f64dd7937e0fd7828d01150cafcb7d
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-11 15:04:49 +0300

    refactor FuelTable and add FuelTableTest

--- src/main/java/by/aurorasoft/fuelsearcher/model/FuelTable.java
+++ src/main/java/by/aurorasoft/fuelsearcher/model/FuelTable.java
@@ -1,12 +1,16 @@
 package by.aurorasoft.fuelsearcher.model;
 
 import by.aurorasoft.fuelsearcher.service.dictionary.Translatable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Value;
 import org.apache.poi.xwpf.usermodel.IBodyElement;
 
 import java.util.List;
 
 @Value
+@AllArgsConstructor
+@Builder
 public class FuelTable implements Translatable {
     String name;
     List<IBodyElement> elements;
---
+++ src/test/java/by/aurorasoft/fuelsearcher/model/FuelTableTest.java
@@ -0,0 +1,20 @@
+package by.aurorasoft.fuelsearcher.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public final class FuelTableTest {
+
+    @Test
+    public void aliasShouldBeFound() {
+        final String givenTableName = "table";
+        final FuelTable givenTable = FuelTable.builder()
+                .name(givenTableName)
+                .build();
+
+        final String actual = givenTable.findAlias();
+        assertEquals(givenTableName, actual);
+    }
+
+}
