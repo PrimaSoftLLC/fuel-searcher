973545d205d95e90f6c66a79f94b374711177369
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 23:24:05 +0300

    refactor SixthNotAcceptableTableFuelSearchingArgumentsProvider

--- src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/notacceptable/SixthNotAcceptableTableFuelSearchingArgumentsProvider.java
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/notacceptable/SixthNotAcceptableTableFuelSearchingArgumentsProvider.java
@@ -3,6 +3,7 @@ package by.aurorasoft.fuelsearcher.it.argumentsprovider.notacceptable;
 import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.NotAcceptableFuelSearchingArguments;
 import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification.FuelSpecificationBuilder;
 
+import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
@@ -22,7 +23,7 @@ public final class SixthNotAcceptableTableFuelSearchingArgumentsProvider extends
                                 .workingWidth("8.8")
                                 .routingLength("Менее 150")
                                 .build())
-                        .failedPropertyNames(new String[]{"глубина обработки", "механизм"})
+                        .failedPropertyNames(List.of("глубина обработки", "механизм"))
                         .build()
         );
     }
