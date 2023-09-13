8be24382bd47348e613620717fc29b1189c2ff19
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 16:15:02 +0300

    add SixteenthNotAcceptableTableFuelSearchingArgumentsProvider

---
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/notacceptable/SixteenthNotAcceptableTableFuelSearchingArgumentsProvider.java
@@ -0,0 +1,29 @@
+package by.aurorasoft.fuelsearcher.it.argumentsprovider.notacceptable;
+
+import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.NotAcceptableFuelSearchingArguments;
+import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification.FuelSpecificationBuilder;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public final class SixteenthNotAcceptableTableFuelSearchingArgumentsProvider extends NotAcceptableTableFuelSearchingArgumentsProvider {
+    private static final String TABLE_NAME = "КОШЕНИЕ ТРАВ С ПЛЮЩЕНИЕМ";
+
+    public SixteenthNotAcceptableTableFuelSearchingArgumentsProvider() {
+        super(TABLE_NAME);
+    }
+
+    @Override
+    protected Stream<NotAcceptableFuelSearchingArguments> createArguments(final Supplier<FuelSpecificationBuilder> specificationBuilderSupplier) {
+        return Stream.of(
+                NotAcceptableFuelSearchingArguments.builder()
+                        .specification(specificationBuilderSupplier.get()
+                                .machinery("Krone-B 1000 CV Collet")
+                                .workingWidth("9.7")
+                                .yield("до 10")
+                                .build())
+                        .failedPropertyNames(new String[]{"трактор", "длина гона"})
+                        .build()
+        );
+    }
+}
