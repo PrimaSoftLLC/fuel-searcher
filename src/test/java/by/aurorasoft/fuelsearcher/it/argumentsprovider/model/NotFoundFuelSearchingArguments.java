92832174e51a84d3e2836fe9db5a71c3a02ded57
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 12:32:46 +0300

    add NotFoundTableFuelSearchingArgumentsProvider

--- src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/model/NotFoundFuelSearchingArguments.java
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/model/NotFoundFuelSearchingArguments.java
@@ -1,9 +1,11 @@
 package by.aurorasoft.fuelsearcher.it.argumentsprovider.model;
 
 import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification;
+import lombok.Builder;
 
 public final class NotFoundFuelSearchingArguments extends FuelSearchingArguments {
 
+    @Builder
     public NotFoundFuelSearchingArguments(final FuelSpecification specification) {
         super(specification);
     }
