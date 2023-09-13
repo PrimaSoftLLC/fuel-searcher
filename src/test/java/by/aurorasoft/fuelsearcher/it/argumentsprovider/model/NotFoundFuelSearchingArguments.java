b2f3e79d4500b3fdc5033a142a1f5e15390ba8df
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 10:33:45 +0300

    add NotFoundFuelSearchingArguments

---
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/model/NotFoundFuelSearchingArguments.java
@@ -0,0 +1,11 @@
+package by.aurorasoft.fuelsearcher.it.argumentsprovider.model;
+
+import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification;
+
+public final class NotFoundFuelSearchingArguments extends FuelSearchingArguments {
+
+    public NotFoundFuelSearchingArguments(final FuelSpecification specification) {
+        super(specification);
+    }
+
+}
