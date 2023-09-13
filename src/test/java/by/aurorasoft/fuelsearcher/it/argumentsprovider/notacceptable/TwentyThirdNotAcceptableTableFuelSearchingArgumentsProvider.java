2e97dcb2d03fc831c3c1def0f88d93d4c1f11a36
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 16:40:37 +0300

    add TwentyThirdNotAcceptableTableFuelSearchingArgumentsProvider

---
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/notacceptable/TwentyThirdNotAcceptableTableFuelSearchingArgumentsProvider.java
@@ -0,0 +1,29 @@
+package by.aurorasoft.fuelsearcher.it.argumentsprovider.notacceptable;
+
+import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.NotAcceptableFuelSearchingArguments;
+import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification.FuelSpecificationBuilder;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public final class TwentyThirdNotAcceptableTableFuelSearchingArgumentsProvider extends NotAcceptableTableFuelSearchingArgumentsProvider {
+    private static final String TABLE_NAME = "ПОДБОР ПРОВЯЛЕННЫХ ТРАВ ИЗ ВАЛКОВ С ИЗМЕЛЬЧЕНИЕМ И ПОДАЧЕЙ В ТРАНСПОРТНЫЕ СРЕДСТВА";
+
+    public TwentyThirdNotAcceptableTableFuelSearchingArgumentsProvider() {
+        super(TABLE_NAME);
+    }
+
+    @Override
+    protected Stream<NotAcceptableFuelSearchingArguments> createArguments(final Supplier<FuelSpecificationBuilder> specificationBuilderSupplier) {
+        return Stream.of(
+                NotAcceptableFuelSearchingArguments.builder()
+                        .specification(specificationBuilderSupplier.get()
+                                .workingWidth("3.8")
+                                .yield("5-7.5")
+                                .routingLength("Менее 150")
+                                .build())
+                        .failedPropertyNames(new String[]{"механизм"})
+                        .build()
+        );
+    }
+}
