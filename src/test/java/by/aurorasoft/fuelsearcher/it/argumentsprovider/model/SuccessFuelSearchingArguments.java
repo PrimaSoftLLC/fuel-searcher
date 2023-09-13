73fdef33dbe1b8968039465ecdc8eadcb538ccaa
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 11:13:00 +0300

    add FirstSuccessTableFuelSearchingArgumentsProvider

--- src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/model/SuccessFuelSearchingArguments.java
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/model/SuccessFuelSearchingArguments.java
@@ -2,12 +2,14 @@ package by.aurorasoft.fuelsearcher.it.argumentsprovider.model;
 
 import by.aurorasoft.fuelsearcher.model.Fuel;
 import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public final class SuccessFuelSearchingArguments extends FuelSearchingArguments {
     private final Fuel expected;
 
+    @Builder
     public SuccessFuelSearchingArguments(final FuelSpecification specification, final Fuel expected) {
         super(specification);
         this.expected = expected;
