e1cc0782fd6a6d7b8db17b38556aa2c9c0f21a1a
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 11:24:38 +0300

    add SixthSuccessTableFuelSearchingArgumentsProvider

---
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/success/SixthSuccessTableFuelSearchingArgumentsProvider.java
@@ -0,0 +1,52 @@
+package by.aurorasoft.fuelsearcher.it.argumentsprovider.success;
+
+import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.SuccessFuelSearchingArguments;
+import by.aurorasoft.fuelsearcher.model.Fuel;
+import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification.FuelSpecificationBuilder;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public final class SixthSuccessTableFuelSearchingArgumentsProvider extends SuccessTableFuelSearchingArgumentsProvider {
+    private static final String TABLE_NAME = "ЛУЩЕНИЕ И ДИСКОВАНИЕ МНОГОЛЕТНИХ ТРАВ";
+
+    public SixthSuccessTableFuelSearchingArgumentsProvider() {
+        super(TABLE_NAME);
+    }
+
+    @Override
+    protected Stream<SuccessFuelSearchingArguments> createArguments(final Supplier<FuelSpecificationBuilder> specificationBuilderSupplier) {
+        return Stream.of(
+                SuccessFuelSearchingArguments.builder()
+                        .specification(specificationBuilderSupplier.get()
+                                .tractor("Кировец К-744Р4")
+                                .machinery("Доминанта Д-880")
+                                .workingWidth("8.8")
+                                .routingLength("Менее 150")
+                                .processingDepth("Глубина обработки 6...8 см")
+                                .build())
+                        .expected(new Fuel(22.9, 12.5))
+                        .build(),
+                SuccessFuelSearchingArguments.builder()
+                        .specification(specificationBuilderSupplier.get()
+                                .tractor("Кировец К-744Р4")
+                                .machinery("АДС-6")
+                                .workingWidth("6")
+                                .routingLength("Менее 150")
+                                .processingDepth("Глубина обработки 6...8 см")
+                                .build())
+                        .expected(new Fuel(15.5, 12.8))
+                        .build(),
+                SuccessFuelSearchingArguments.builder()
+                        .specification(specificationBuilderSupplier.get()
+                                .tractor("Беларус 2022")
+                                .machinery("АКЧ-6")
+                                .workingWidth("6")
+                                .routingLength("150-200")
+                                .processingDepth("Глубина обработки 10...14 см")
+                                .build())
+                        .expected(new Fuel(15.4, 7.7))
+                        .build()
+        );
+    }
+}
