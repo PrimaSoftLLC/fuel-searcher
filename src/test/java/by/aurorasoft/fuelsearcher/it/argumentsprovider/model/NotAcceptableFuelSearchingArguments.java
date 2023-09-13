51e397734c15780410976d538dcc52f8732ae5f8
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 14:47:40 +0300

    add FirstNotAcceptableTableFuelSearchingArgumentsProvider

--- src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/model/NotAcceptableFuelSearchingArguments.java
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/model/NotAcceptableFuelSearchingArguments.java
@@ -1,12 +1,14 @@
 package by.aurorasoft.fuelsearcher.it.argumentsprovider.model;
 
 import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public final class NotAcceptableFuelSearchingArguments extends FuelSearchingArguments {
     private final String[] failedPropertyNames;
 
+    @Builder
     public NotAcceptableFuelSearchingArguments(final FuelSpecification specification,
                                                final String[] failedPropertyNames) {
         super(specification);
---
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/notacceptable/FirstNotAcceptableTableFuelSearchingArgumentsProvider.java
@@ -0,0 +1,29 @@
+package by.aurorasoft.fuelsearcher.it.argumentsprovider.notacceptable;
+
+import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.NotAcceptableFuelSearchingArguments;
+import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification.FuelSpecificationBuilder;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public final class FirstNotAcceptableTableFuelSearchingArgumentsProvider extends NotAcceptableTableFuelSearchingArgumentsProvider {
+    private static final String TABLE_NAME = "ВСПАШКА ПЛАСТА МНОГОЛЕТНИХ ТРАВ";
+
+    public FirstNotAcceptableTableFuelSearchingArgumentsProvider() {
+        super(TABLE_NAME);
+    }
+
+    @Override
+    protected Stream<NotAcceptableFuelSearchingArguments> createArguments(final Supplier<FuelSpecificationBuilder> specificationBuilderSupplier) {
+        return Stream.of(
+                NotAcceptableFuelSearchingArguments.builder()
+                        .specification(specificationBuilderSupplier.get()
+                                .ploughingDepth("18-20")
+                                .routingLength("Менее 150")
+                                .specificResistance("Удельное сопротивление 48...53 кПа")
+                                .build())
+                        .failedPropertyNames(new String[]{"трактор", "механизм", "количество корпусов"})
+                        .build()
+        );
+    }
+}
