78e44a0d6ea54a3ba2f8dcae4f54f55e4e5c6258
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 11:11:02 +0300

    refactor SuccessTableFuelSearchingArgumentsProvider

--- src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/success/SuccessTableFuelSearchingArgumentsProvider.java
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/success/SuccessTableFuelSearchingArgumentsProvider.java
@@ -3,11 +3,20 @@ package by.aurorasoft.fuelsearcher.it.argumentsprovider.success;
 import by.aurorasoft.fuelsearcher.it.argumentsprovider.TableFuelSearchingArgumentsProvider;
 import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.SuccessFuelSearchingArguments;
 
+import java.util.stream.Stream;
+
 public abstract class SuccessTableFuelSearchingArgumentsProvider
         extends TableFuelSearchingArgumentsProvider<SuccessFuelSearchingArguments> {
 
     public SuccessTableFuelSearchingArgumentsProvider(final String tableName) {
         super(tableName);
+    }
+
+    @Override
+    protected final Stream<Object> findAdditionalJunitArgumentComponents(final SuccessFuelSearchingArguments arguments) {
+        return Stream.of(
+                arguments.getExpected()
+        );
     }
 
 }
