40a07873b488c83f6c565924b1a4542354f54652
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 17:20:21 +0300

    add TwentySeventhNotAcceptableTableFuelSearchingArgumentsProvider

---
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/notacceptable/TwentySeventhNotAcceptableTableFuelSearchingArgumentsProvider.java
@@ -0,0 +1,29 @@
+package by.aurorasoft.fuelsearcher.it.argumentsprovider.notacceptable;
+
+import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.NotAcceptableFuelSearchingArguments;
+import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification.FuelSpecificationBuilder;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public final class TwentySeventhNotAcceptableTableFuelSearchingArgumentsProvider extends NotAcceptableTableFuelSearchingArgumentsProvider {
+    private static final String TABLE_NAME = "ТРАНСПОРТИРОВКА ГРУЗОВ ТРАКТОРАМИ С ОДНИМ ПРИЦЕПОМ";
+
+    public TwentySeventhNotAcceptableTableFuelSearchingArgumentsProvider() {
+        super(TABLE_NAME);
+    }
+
+    @Override
+    protected Stream<NotAcceptableFuelSearchingArguments> createArguments(final Supplier<FuelSpecificationBuilder> specificationBuilderSupplier) {
+        return Stream.of(
+                NotAcceptableFuelSearchingArguments.builder()
+                        .specification(specificationBuilderSupplier.get()
+                                .tractor("БЕЛАРУС-3522")
+                                .machinery("ПСС-25")
+                                .transportDistance("0.25-0.75")
+                                .build())
+                        .failedPropertyNames(new String[]{"класс груза", "группа дорог"})
+                        .build()
+        );
+    }
+}
