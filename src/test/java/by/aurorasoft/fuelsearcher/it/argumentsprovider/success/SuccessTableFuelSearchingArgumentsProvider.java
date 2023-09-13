a9692e25cb128a29b19652aa7eb7aca6666d103c
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 11:08:34 +0300

    add SuccessTableFuelSearchingArgumentsProvider

---
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/success/SuccessTableFuelSearchingArgumentsProvider.java
@@ -0,0 +1,13 @@
+package by.aurorasoft.fuelsearcher.it.argumentsprovider.success;
+
+import by.aurorasoft.fuelsearcher.it.argumentsprovider.TableFuelSearchingArgumentsProvider;
+import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.SuccessFuelSearchingArguments;
+
+public abstract class SuccessTableFuelSearchingArgumentsProvider
+        extends TableFuelSearchingArgumentsProvider<SuccessFuelSearchingArguments> {
+
+    public SuccessTableFuelSearchingArgumentsProvider(final String tableName) {
+        super(tableName);
+    }
+
+}
