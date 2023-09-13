c31f59eb306d1107dfaa504dbbba24a679762f50
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 23:19:08 +0300

    refactor TenthNotAcceptableTableFuelSearchingArgumentsProvider

--- src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/notacceptable/TenthNotAcceptableTableFuelSearchingArgumentsProvider.java
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/notacceptable/TenthNotAcceptableTableFuelSearchingArgumentsProvider.java
@@ -3,6 +3,7 @@ package by.aurorasoft.fuelsearcher.it.argumentsprovider.notacceptable;
 import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.NotAcceptableFuelSearchingArguments;
 import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification.FuelSpecificationBuilder;
 
+import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
@@ -22,7 +23,7 @@ public final class TenthNotAcceptableTableFuelSearchingArgumentsProvider extends
                                 .machinery("Амкодор \"Veras\" 12000")
                                 .sowingNorm("Норма высева семян 3 кг/га")
                                 .build())
-                        .failedPropertyNames(new String[]{"ширина захвата", "длина гона"})
+                        .failedPropertyNames(List.of("ширина захвата", "длина гона"))
                         .build()
         );
     }
