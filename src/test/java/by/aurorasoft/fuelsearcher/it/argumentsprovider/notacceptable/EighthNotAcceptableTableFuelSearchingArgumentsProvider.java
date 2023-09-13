d5e54b8cc9b9daba301cf098eaeda3c523a7dd1b
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 15:09:18 +0300

    add EighthNotAcceptableTableFuelSearchingArgumentsProvider

---
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/notacceptable/EighthNotAcceptableTableFuelSearchingArgumentsProvider.java
@@ -0,0 +1,29 @@
+package by.aurorasoft.fuelsearcher.it.argumentsprovider.notacceptable;
+
+import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.NotAcceptableFuelSearchingArguments;
+import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification.FuelSpecificationBuilder;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public final class EighthNotAcceptableTableFuelSearchingArgumentsProvider extends NotAcceptableTableFuelSearchingArgumentsProvider {
+    private static final String TABLE_NAME = "ПОСЕВ КУКУРУЗЫ, ПОДСОЛНЕЧНИКА";
+
+    public EighthNotAcceptableTableFuelSearchingArgumentsProvider() {
+        super(TABLE_NAME);
+    }
+
+    @Override
+    protected Stream<NotAcceptableFuelSearchingArguments> createArguments(final Supplier<FuelSpecificationBuilder> specificationBuilderSupplier) {
+        return Stream.of(
+                NotAcceptableFuelSearchingArguments.builder()
+                        .specification(specificationBuilderSupplier.get()
+                                .tractor("Беларус 3522")
+                                .routingLength("Менее 150")
+                                .sowingNorm("Норма высева 15 кг/га")
+                                .build())
+                        .failedPropertyNames(new String[]{"ширина захвата", "механизм"})
+                        .build()
+        );
+    }
+}
