d0b78bdfef6fe780b38c6413276e554152dc1af2
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-13 12:10:51 +0300

    add NineteenthSuccessTableFuelSearchingArgumentsProvider

---
+++ src/test/java/by/aurorasoft/fuelsearcher/it/argumentsprovider/success/NineteenthSuccessTableFuelSearchingArgumentsProvider.java
@@ -0,0 +1,52 @@
+package by.aurorasoft.fuelsearcher.it.argumentsprovider.success;
+
+import by.aurorasoft.fuelsearcher.it.argumentsprovider.model.SuccessFuelSearchingArguments;
+import by.aurorasoft.fuelsearcher.model.Fuel;
+import by.aurorasoft.fuelsearcher.model.specification.FuelSpecification.FuelSpecificationBuilder;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public final class NineteenthSuccessTableFuelSearchingArgumentsProvider extends SuccessTableFuelSearchingArgumentsProvider {
+    private static final String TABLE_NAME = "ПРЕССОВАНИЕ СЕНА ПОСЛЕ КОМБАЙНА";
+
+    public NineteenthSuccessTableFuelSearchingArgumentsProvider() {
+        super(TABLE_NAME);
+    }
+
+    @Override
+    protected Stream<SuccessFuelSearchingArguments> createArguments(final Supplier<FuelSpecificationBuilder> specificationBuilderSupplier) {
+        return Stream.of(
+                SuccessFuelSearchingArguments.builder()
+                        .specification(specificationBuilderSupplier.get()
+                                .tractor("Fendt 515c")
+                                .machinery("KRONE Comprima CV 150 XC")
+                                .workingWidth("5.5")
+                                .yield("До 1")
+                                .routingLength("Менее 150")
+                                .build())
+                        .expected(new Fuel(12.8, 4.35))
+                        .build(),
+                SuccessFuelSearchingArguments.builder()
+                        .specification(specificationBuilderSupplier.get()
+                                .tractor("Fendt 515c")
+                                .machinery("KRONE Comprima CV 150 XC")
+                                .workingWidth("5.5")
+                                .yield("4.6-5")
+                                .routingLength("150-200")
+                                .build())
+                        .expected(new Fuel(50.7, 1.17))
+                        .build(),
+                SuccessFuelSearchingArguments.builder()
+                        .specification(specificationBuilderSupplier.get()
+                                .tractor("Fendt 515c")
+                                .machinery("KRONE Comprima CV 150 XC")
+                                .workingWidth("5.5")
+                                .yield("4.1-4.5")
+                                .routingLength("150-200")
+                                .build())
+                        .expected(new Fuel(48.6, 1.21))
+                        .build()
+        );
+    }
+}
