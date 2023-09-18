7f5335d9eb4cb0807d5d68040f9e2e28443fe8c7
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-18 12:20:19 +0300

    add FuelTableController

---
+++ src/main/java/by/aurorasoft/fuelsearcher/controller/FuelTableController.java
@@ -0,0 +1,34 @@
+package by.aurorasoft.fuelsearcher.controller;
+
+import by.aurorasoft.fuelsearcher.model.FuelDocument;
+import by.aurorasoft.fuelsearcher.model.FuelTable;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+@RestController
+@RequestMapping("fuelTable")
+@RequiredArgsConstructor
+public class FuelTableController {
+    private final FuelDocument document;
+
+    @GetMapping("tableNames")
+    public ResponseEntity<List<String>> findTableNames() {
+        final List<String> tableNames = this.findAllTableNames();
+        return ok(tableNames);
+    }
+
+    private List<String> findAllTableNames() {
+        return this.document.tables()
+                .stream()
+                .map(FuelTable::name)
+                .toList();
+    }
+
+}
