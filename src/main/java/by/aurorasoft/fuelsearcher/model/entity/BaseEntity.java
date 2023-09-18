29f618712fdf72567e60af4e1b2e447eb7429c16
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-18 15:20:15 +0300

    add BaseEntity

---
+++ src/main/java/by/aurorasoft/fuelsearcher/model/entity/BaseEntity.java
@@ -0,0 +1,34 @@
+package by.aurorasoft.fuelsearcher.model.entity;
+
+import by.nhorushko.crudgeneric.v2.domain.AbstractEntity;
+
+import java.util.Objects;
+
+public abstract class BaseEntity<IdType> implements AbstractEntity<IdType> {
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public final boolean equals(final Object otherObject) {
+        if (this == otherObject) {
+            return true;
+        }
+        if (otherObject == null) {
+            return false;
+        }
+        if (this.getClass() != otherObject.getClass()) {
+            return false;
+        }
+        final BaseEntity<IdType> other = (BaseEntity<IdType>) otherObject;
+        return Objects.equals(this.getId(), other.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hashCode(this.getId());
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getName() + "[id = " + this.getId() + "]";
+    }
+}
