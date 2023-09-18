b468069894bc1f85abb82c73975d69b8077ade10
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-18 15:42:16 +0300

    add TableMetadataRepository

---
+++ src/main/java/by/aurorasoft/fuelsearcher/repository/TableMetadataRepository.java
@@ -0,0 +1,7 @@
+package by.aurorasoft.fuelsearcher.repository;
+
+import by.aurorasoft.fuelsearcher.model.entity.TableMetadataEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TableMetadataRepository extends JpaRepository<TableMetadataEntity, Long> {
+}
