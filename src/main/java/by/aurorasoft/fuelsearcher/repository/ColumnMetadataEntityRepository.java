e78d54d1db6d6df30059ed4c1542e270314714d3
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-18 15:43:37 +0300

    add ColumnMetadataEntityRepository

---
+++ src/main/java/by/aurorasoft/fuelsearcher/repository/ColumnMetadataEntityRepository.java
@@ -0,0 +1,7 @@
+package by.aurorasoft.fuelsearcher.repository;
+
+import by.aurorasoft.fuelsearcher.model.entity.ColumnMetadataEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ColumnMetadataEntityRepository extends JpaRepository<ColumnMetadataEntity, Long> {
+}
