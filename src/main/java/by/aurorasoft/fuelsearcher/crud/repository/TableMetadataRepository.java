901f4e57e188fb72e801d8bfe9c3dd36fd75e943
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-19 10:43:46 +0300

    add TableMetadataRepository::findTableMetadata

--- src/main/java/by/aurorasoft/fuelsearcher/crud/repository/TableMetadataRepository.java
+++ src/main/java/by/aurorasoft/fuelsearcher/crud/repository/TableMetadataRepository.java
@@ -2,6 +2,12 @@ package by.aurorasoft.fuelsearcher.crud.repository;
 
 import by.aurorasoft.fuelsearcher.crud.model.entity.TableMetadataEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface TableMetadataRepository extends JpaRepository<TableMetadataEntity, Long> {
+
+    @Query("SELECT e FROM TableMetadataEntity e JOIN FETCH e.propertiesMetadata WHERE e.tableName = :tableName")
+    Optional<TableMetadataEntity> findTableMetadata(final String tableName);
 }
