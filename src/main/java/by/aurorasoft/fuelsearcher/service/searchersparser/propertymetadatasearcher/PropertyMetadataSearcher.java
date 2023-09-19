141b9fe2279f3f9bf10bf9e38e89604f4465a013
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-19 15:41:47 +0300

    add PropertyMetadataSearcher

--- src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/propertymetadatasearcher/PropertyMetadataSearcher.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/propertymetadatasearcher/PropertyMetadataSearcher.java
@@ -4,7 +4,9 @@ import by.aurorasoft.fuelsearcher.crud.model.dto.PropertyMetadata;
 import by.aurorasoft.fuelsearcher.model.FuelTable;
 import lombok.RequiredArgsConstructor;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 @RequiredArgsConstructor
@@ -21,14 +23,16 @@ public abstract class PropertyMetadataSearcher {
                 .build();
     }
 
-    protected abstract Stream<String> findPropertyValuesInSubTable(final XWPFTable subTable, final int cellIndex);
+    protected abstract Stream<String> findPropertyValuesInSubTable(final List<XWPFTableRow> subTableRows,
+                                                                   final int cellIndex);
 
     private String[] findPropertyValuesInSubTables() {
         return this.fuelTable.elements()
                 .stream()
                 .filter(XWPFTable.class::isInstance)
                 .map(XWPFTable.class::cast)
-                .flatMap(subTable -> this.findPropertyValuesInSubTable(subTable, cellIndex))
+                .map(XWPFTable::getRows)
+                .flatMap(subTableRows -> this.findPropertyValuesInSubTable(subTableRows, this.cellIndex))
                 .distinct()
                 .toArray(String[]::new);
     }
