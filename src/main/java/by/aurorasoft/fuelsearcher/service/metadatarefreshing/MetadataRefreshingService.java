6e7bc2847c4f8811b3722989782efa293c563894
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-19 14:21:05 +0300

    add MetadataRefreshingService

--- src/main/java/by/aurorasoft/fuelsearcher/service/metadatarefreshing/MetadataRefreshingService.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/metadatarefreshing/MetadataRefreshingService.java
@@ -1,11 +1,15 @@
 package by.aurorasoft.fuelsearcher.service.metadatarefreshing;
 
+import by.aurorasoft.fuelsearcher.crud.model.dto.TableMetadata;
 import by.aurorasoft.fuelsearcher.crud.service.TableMetadataService;
 import by.aurorasoft.fuelsearcher.service.searchersparser.SearchersParsingResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+//TODO: test
 @Service
 @RequiredArgsConstructor
 public class MetadataRefreshingService {
@@ -14,6 +18,12 @@ public class MetadataRefreshingService {
 
     @Transactional
     public void refresh() {
+        this.tableMetadataService.deleteAll();
+        this.saveNewMetadata();
+    }
 
+    private void saveNewMetadata() {
+        final List<TableMetadata> newMetadata = this.parsingResult.tablesMetadata();
+        this.tableMetadataService.saveAll(newMetadata);
     }
 }
