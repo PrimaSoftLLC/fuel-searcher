405602432b6e46fa402271f9021a50630b2686f5
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-20 15:48:26 +0300

    add PropertyMetadataSearchingManager

--- src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/metadatasearcher/PropertyMetadataSearchingManager.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/metadatasearcher/PropertyMetadataSearchingManager.java
@@ -1,5 +1,7 @@
 package by.aurorasoft.fuelsearcher.service.searchersparser.metadatasearcher;
 
+import by.aurorasoft.fuelsearcher.crud.model.dto.PropertyMetadata;
+import by.aurorasoft.fuelsearcher.model.FuelTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +10,20 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public final class PropertyMetadataSearchingManager {
+    private static final String EXCEPTION_DESCRIPTION_NOT_SUITABLE_SOURCE
+            = "Impossible to find metadata of property by given source '%s'";
+
     private final List<PropertyMetadataSearcher<?>> searchers;
 
-
+    public PropertyMetadata find(final FuelTable fuelTable, final Object source) {
+        return this.searchers.stream()
+                .filter(searcher -> searcher.isAbleToFind(source))
+                .findFirst()
+                .map(searcher -> searcher.find(fuelTable, source))
+                .orElseThrow(
+                        () -> new IllegalArgumentException(
+                                EXCEPTION_DESCRIPTION_NOT_SUITABLE_SOURCE.formatted(source)
+                        )
+                );
+    }
 }
