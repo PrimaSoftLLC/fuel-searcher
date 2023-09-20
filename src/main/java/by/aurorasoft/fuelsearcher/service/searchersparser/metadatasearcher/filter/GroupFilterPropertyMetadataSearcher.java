c5591a5e346e7a68e04a5bf337790eb72013d315
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-20 14:34:14 +0300

    refactor GroupFilterPropertyMetadataSearcher

--- src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/metadatasearcher/filter/GroupFilterPropertyMetadataSearcher.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/metadatasearcher/filter/GroupFilterPropertyMetadataSearcher.java
@@ -2,13 +2,20 @@ package by.aurorasoft.fuelsearcher.service.searchersparser.metadatasearcher.filt
 
 import by.aurorasoft.fuelsearcher.model.filter.interim.group.GroupFilter;
 import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Stream;
 
 import static by.aurorasoft.fuelsearcher.util.XWPFTableRowUtil.isCellTextMatchRegex;
 
+//TODO: test
+@Component
 public final class GroupFilterPropertyMetadataSearcher extends FilterPropertyMetadataSearcher<GroupFilter> {
+
+    public GroupFilterPropertyMetadataSearcher() {
+        super(GroupFilter.class);
+    }
 
     @Override
     protected Stream<XWPFTableRow> findRowsWithPropertyValues(final List<XWPFTableRow> subTableDataRows,
