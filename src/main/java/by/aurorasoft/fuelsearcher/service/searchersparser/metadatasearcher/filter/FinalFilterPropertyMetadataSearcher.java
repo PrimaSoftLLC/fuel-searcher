c265c1c854468be8588b541c5a4b844238ad6635
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-21 10:25:46 +0300

    refactor FinalFilterPropertyMetadataSearcher

--- src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/metadatasearcher/filter/FinalFilterPropertyMetadataSearcher.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/metadatasearcher/filter/FinalFilterPropertyMetadataSearcher.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static by.aurorasoft.fuelsearcher.util.XWPFTableRowUtil.isCellNullOrEmpty;
+import static by.aurorasoft.fuelsearcher.util.XWPFTableRowFilteringUtil.findRowsWithNotNullAndNotEmptyCell;
 
 @Component
 public final class FinalFilterPropertyMetadataSearcher extends FilterPropertyMetadataSearcher<FinalFilter> {
@@ -20,12 +20,9 @@ public final class FinalFilterPropertyMetadataSearcher extends FilterPropertyMet
     protected Stream<XWPFTableRow> findRowsWithPropertyValues(final List<XWPFTableRow> subTableDataRows,
                                                               final FinalFilter filter) {
         final int filtrationCellIndex = filter.getFiltrationCellIndex();
-        return subTableDataRows.stream()
-                .filter(
-                        row -> !isCellNullOrEmpty(
-                                row,
-                                filtrationCellIndex
-                        )
-                );
+        return findRowsWithNotNullAndNotEmptyCell(
+                subTableDataRows,
+                filtrationCellIndex
+        );
     }
 }
