116507ccf1dc9910dff568e652c453b27fb6df83
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-22 11:59:46 +0300

    refactor SearchersParsingHandlerFactory

--- src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/handler/SearchersParsingHandlerFactory.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/handler/SearchersParsingHandlerFactory.java
@@ -2,16 +2,18 @@ package by.aurorasoft.fuelsearcher.service.searchersparser.handler;
 
 import by.aurorasoft.fuelsearcher.service.dictionary.TagHandlerDictionary;
 import by.aurorasoft.fuelsearcher.service.searchersparser.handler.context.SearchersParsingContext;
+import by.aurorasoft.fuelsearcher.service.searchersparser.handler.context.SearchersParsingContextFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public final class SearchersParsingHandlerFactory {
+    private final SearchersParsingContextFactory contextFactory;
     private final TagHandlerDictionary handlerDictionary;
 
     public SearchersParsingHandler create() {
-        final SearchersParsingContext context = new SearchersParsingContext();
+        final SearchersParsingContext context = this.contextFactory.create();
         return new SearchersParsingHandler(this.handlerDictionary, context);
     }
 }
