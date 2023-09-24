f51047c3c8527555f8778de69c0904da3e4c32a3
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-25 00:31:34 +0300

    refactor SubTableTitleMetadata

--- src/main/java/by/aurorasoft/fuelsearcher/model/SubTableTitleMetadata.java
+++ src/main/java/by/aurorasoft/fuelsearcher/model/SubTableTitleMetadata.java
@@ -10,25 +10,59 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
+import static java.util.stream.IntStream.range;
 import static lombok.AccessLevel.PRIVATE;
 
+//TODO: refactor and refactor tests
 @Value
 @AllArgsConstructor(access = PRIVATE)
 public class SubTableTitleMetadata {
-    String template;
+    String templateWithStringFillers;
+    String regex;
     List<SpecificationPropertyExtractor> argumentExtractors;
+
+    public List<SubTableTitlePropertyMetadata> findPropertiesMetadata() {
+        return range(0, this.argumentExtractors.size())
+                .mapToObj(
+                        i -> new SubTableTitlePropertyMetadata(
+                                i,
+                                this.argumentExtractors.get(i)
+                        )
+                )
+                .toList();
+    }
 
     public static SubTableTitleMetadataBuilder builder() {
         return new SubTableTitleMetadataBuilder();
     }
 
+    @Value
+    @AllArgsConstructor(access = PRIVATE)
+    public class SubTableTitlePropertyMetadata {
+        int index;
+        SpecificationPropertyExtractor extractor;
+
+        public String findPropertyName() {
+            return this.extractor.getPropertyName();
+        }
+
+        public String findTitleRegex() {
+            return SubTableTitleMetadata.this.regex;
+        }
+
+        public int findGroupIndexInRegex() {
+            return this.index + 1;
+        }
+    }
+
+    //TODO: stop in refactoring it
     @NoArgsConstructor(access = PRIVATE)
     public static final class SubTableTitleMetadataBuilder extends BuilderRequiringAllProperties<SubTableTitleMetadata> {
-        private String template;
+        private String templateWithPropertyNames;
         private List<SpecificationPropertyExtractor> argumentExtractors;
 
         public void template(final String template) {
-            this.template = template;
+            this.templateWithPropertyNames = template;
         }
 
         public void argumentExtractor(final SpecificationPropertyExtractor extractor) {
@@ -38,12 +72,12 @@ public class SubTableTitleMetadata {
 
         @Override
         protected Stream<Object> findProperties() {
-            return Stream.of(this.template, this.argumentExtractors);
+            return Stream.of(this.templateWithPropertyNames, this.argumentExtractors);
         }
 
         @Override
         protected SubTableTitleMetadata buildAfterStateValidation() {
-            return new SubTableTitleMetadata(this.template, this.argumentExtractors);
+            return new SubTableTitleMetadata(this.templateWithPropertyNames, this.argumentExtractors);
         }
 
         private void initializeArgumentExtractorsIfNecessary() {
