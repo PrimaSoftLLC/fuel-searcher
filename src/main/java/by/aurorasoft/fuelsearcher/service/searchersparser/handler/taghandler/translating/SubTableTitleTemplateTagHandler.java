163e0aa46638bc6f786854f5035b99c549723624
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-25 10:35:01 +0300

    refactor SubTableTitleTemplateTagHandler

--- src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/handler/taghandler/translating/SubTableTitleTemplateTagHandler.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/handler/taghandler/translating/SubTableTitleTemplateTagHandler.java
@@ -6,20 +6,13 @@ import by.aurorasoft.fuelsearcher.service.searchersparser.handler.context.Search
 import by.aurorasoft.fuelsearcher.service.searchersparser.handler.taghandler.translating.exception.NoSuchKeyException;
 import org.springframework.stereotype.Component;
 
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
-import static java.util.regex.Pattern.compile;
+import static by.aurorasoft.fuelsearcher.util.SubTableTitleUtil.findPropertyNames;
 
 @Component
 public final class SubTableTitleTemplateTagHandler extends TranslatingTagHandler<SpecificationPropertyExtractor> {
     private static final String TAG_NAME = "sub-table-title-template";
-
-    private static final String PROPERTY_PLACE_REGEX = "\\{([^{}]+)}";
-    private static final int PROPERTY_GROUP_NUMBER = 1;
-    private static final Pattern PROPERTY_PLACE_PATTERN = compile(PROPERTY_PLACE_REGEX);
-
-    private static final String STRING_FILLER = "%s";
 
     public SubTableTitleTemplateTagHandler(final SpecificationPropertyExtractorDictionary dictionary) {
         super(TAG_NAME, dictionary, NoSuchSpecificationPropertyExtractorException::new);
@@ -32,10 +25,8 @@ public final class SubTableTitleTemplateTagHandler extends TranslatingTagHandler
 
     @Override
     protected Stream<String> findAliases(final SearchersParsingContext context) {
-        final String initialSubTableTitleTemplate = context.getLastContent();
-        return PROPERTY_PLACE_PATTERN.matcher(initialSubTableTitleTemplate)
-                .results()
-                .map(matchResult -> matchResult.group(PROPERTY_GROUP_NUMBER));
+        final String subTableTitleTemplateWithPropertyNames = context.getLastContent();
+        return findPropertyNames(subTableTitleTemplateWithPropertyNames);
     }
 
     @Override
@@ -46,13 +37,8 @@ public final class SubTableTitleTemplateTagHandler extends TranslatingTagHandler
 
     @Override
     protected void accumulateAdditionalValues(final SearchersParsingContext context) {
-        final String subTableTitleTemplate = findSubTableTitleTemplateWithStringFillers(context);
-        context.accumulateSubTableTitleTemplate(subTableTitleTemplate);
-    }
-
-    private static String findSubTableTitleTemplateWithStringFillers(final SearchersParsingContext context) {
-        final String initialSubTableTitleTemplate = context.getLastContent();
-        return initialSubTableTitleTemplate.replaceAll(PROPERTY_PLACE_REGEX, STRING_FILLER);
+        final String subTableTitleTemplateWithPropertyNames = context.getLastContent();
+        context.accumulateSubTableTitleTemplate(subTableTitleTemplateWithPropertyNames);
     }
 
     private static final class NoSuchSpecificationPropertyExtractorException extends NoSuchKeyException {
