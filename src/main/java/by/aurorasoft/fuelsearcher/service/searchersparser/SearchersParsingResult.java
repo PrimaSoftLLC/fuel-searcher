20a1558f8541bae3040fafa4c6c659afabfdae8f
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-26 17:56:36 +0300

    refactor SearchersParsingResult

--- src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/SearchersParsingResult.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/searchersparser/SearchersParsingResult.java
@@ -3,10 +3,30 @@ package by.aurorasoft.fuelsearcher.service.searchersparser;
 import by.aurorasoft.fuelsearcher.crud.model.dto.TableMetadata;
 import by.aurorasoft.fuelsearcher.service.searcher.FuelSearcher;
 import by.aurorasoft.fuelsearcher.service.validator.SpecificationValidator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
+import java.util.Optional;
 
-public record SearchersParsingResult(List<FuelSearcher> searchers,
-                                     List<SpecificationValidator> specificationValidators,
-                                     List<TableMetadata> tablesMetadata) {
+import static java.util.Optional.ofNullable;
+import static lombok.AccessLevel.NONE;
+import static lombok.AccessLevel.PRIVATE;
+
+@AllArgsConstructor
+@Getter
+public final class SearchersParsingResult {
+    private final List<FuelSearcher> searchers;
+    private final List<SpecificationValidator> specificationValidators;
+
+    @Getter(value = NONE)
+    @Setter(value = PRIVATE)
+    private List<TableMetadata> tablesMetadata;
+
+    public Optional<List<TableMetadata>> takeAwayTablesMetadata() {
+        final Optional<List<TableMetadata>> optionalTablesMetadata = ofNullable(this.tablesMetadata);
+        optionalTablesMetadata.ifPresent(tableMetadata -> this.setTablesMetadata(null));
+        return optionalTablesMetadata;
+    }
 }
