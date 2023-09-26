d80f1a77932f38f21a73e34387ab1d1cb63d1da1
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-27 00:04:53 +0300

    refactor SearchersParsingHandlerFactoryTest

--- src/test/java/by/aurorasoft/fuelsearcher/service/searchersparser/handler/SearchersParsingHandlerFactoryTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/service/searchersparser/handler/SearchersParsingHandlerFactoryTest.java
@@ -9,8 +9,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.lang.reflect.Field;
-
+import static by.aurorasoft.fuelsearcher.testutil.ReflectionUtil.findProperty;
 import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -53,25 +52,21 @@ public final class SearchersParsingHandlerFactoryTest {
 
     private static TagHandlerDictionary findTagHandlerDictionary(final SearchersParsingHandler handler)
             throws Exception {
-        return findHandlerProperty(handler, HANDLER_FIELD_NAME_TAG_HANDLER_DICTIONARY, TagHandlerDictionary.class);
+        return findProperty(
+                handler,
+                SearchersParsingHandler.class,
+                HANDLER_FIELD_NAME_TAG_HANDLER_DICTIONARY,
+                TagHandlerDictionary.class
+        );
     }
 
     private static SearchersParsingContext findContext(final SearchersParsingHandler handler)
             throws Exception {
-        return findHandlerProperty(handler, HANDLER_FIELD_NAME_CONTEXT, SearchersParsingContext.class);
-    }
-
-    private static <T> T findHandlerProperty(final SearchersParsingHandler handler,
-                                             final String fieldName,
-                                             final Class<T> propertyType)
-            throws Exception {
-        final Field field = SearchersParsingHandler.class.getDeclaredField(fieldName);
-        field.setAccessible(true);
-        try {
-            final Object property = field.get(handler);
-            return propertyType.cast(property);
-        } finally {
-            field.setAccessible(false);
-        }
+        return findProperty(
+                handler,
+                SearchersParsingHandler.class,
+                HANDLER_FIELD_NAME_CONTEXT,
+                SearchersParsingContext.class
+        );
     }
 }
