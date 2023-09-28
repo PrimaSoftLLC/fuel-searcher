06bb52b0237ce72c1f94a138d02d7445be2c2da5
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 01:09:37 +0300

    add SpreadRateHeaderMetadataFactory

---
+++ src/main/java/by/aurorasoft/fuelsearcher/service/factory/fuelheadermetadata/SpreadRateHeaderMetadataFactory.java
@@ -0,0 +1,21 @@
+package by.aurorasoft.fuelsearcher.service.factory.fuelheadermetadata;
+
+import by.aurorasoft.fuelsearcher.model.header.SpreadRateHeaderMetadata;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.SpreadRateExtractor;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public final class SpreadRateHeaderMetadataFactory extends FuelHeaderMetadataFactory<SpreadRateExtractor> {
+
+    public SpreadRateHeaderMetadataFactory(final SpreadRateExtractor spreadRateExtractor) {
+        super(spreadRateExtractor);
+    }
+
+    @Override
+    protected SpreadRateHeaderMetadata create(final SpreadRateExtractor spreadRateExtractor,
+                                              final Map<String, Integer> fuelOffsetsByHeaders) {
+        return new SpreadRateHeaderMetadata(spreadRateExtractor, fuelOffsetsByHeaders);
+    }
+}
