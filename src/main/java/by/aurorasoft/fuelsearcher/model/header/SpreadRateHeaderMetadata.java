106f541c7f85eabebba4080c6452ff63556652a3
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 01:07:37 +0300

    refactor SpreadRateHeaderMetadata

--- src/main/java/by/aurorasoft/fuelsearcher/model/header/SpreadRateHeaderMetadata.java
+++ src/main/java/by/aurorasoft/fuelsearcher/model/header/SpreadRateHeaderMetadata.java
@@ -1,15 +1,14 @@
 package by.aurorasoft.fuelsearcher.model.header;
 
 import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.SpreadRateExtractor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
-@Component
+import java.util.Map;
+
 public final class SpreadRateHeaderMetadata extends FuelHeaderMetadata {
 
-    public SpreadRateHeaderMetadata(@Value("${spread-rate}") final String[] values,
-                                    final SpreadRateExtractor spreadRateExtractor) {
-        super(values, spreadRateExtractor);
+    public SpreadRateHeaderMetadata(final SpreadRateExtractor spreadRateExtractor,
+                                    final Map<String, Integer> fuelOffsetsByValues) {
+        super(spreadRateExtractor, fuelOffsetsByValues);
     }
 
 }
