48f542891c8cbb0cd1694fb433a347a7e70652f5
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 00:27:30 +0300

    refactor FuelSearcherDictionary

--- src/main/java/by/aurorasoft/fuelsearcher/service/dictionary/FuelSearcherDictionary.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/dictionary/FuelSearcherDictionary.java
@@ -3,11 +3,13 @@ package by.aurorasoft.fuelsearcher.service.dictionary;
 import by.aurorasoft.fuelsearcher.service.searcher.FuelSearcher;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public final class FuelSearcherDictionary extends Dictionary<FuelSearcher> {
 
-    public FuelSearcherDictionary(final SearchersParsingResult searchersParsingResult) {
-        super(searchersParsingResult.getSearchers());
+    public FuelSearcherDictionary(final List<FuelSearcher> searchers) {
+        super(searchers);
     }
 
 }
