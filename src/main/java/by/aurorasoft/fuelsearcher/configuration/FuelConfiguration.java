9745e12f7f22949c21fad40b5d95f04cfde0a123
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 00:33:07 +0300

    refactor specification validators initialization

--- src/main/java/by/aurorasoft/fuelsearcher/configuration/FuelConfiguration.java
+++ src/main/java/by/aurorasoft/fuelsearcher/configuration/FuelConfiguration.java
@@ -1,9 +1,11 @@
 package by.aurorasoft.fuelsearcher.configuration;
 
 import by.aurorasoft.fuelsearcher.model.FuelDocument;
+import by.aurorasoft.fuelsearcher.service.derivingsearcherfactory.SpecificationValidatorsFactory;
 import by.aurorasoft.fuelsearcher.service.documentfactory.FuelDocumentFactory;
 import by.aurorasoft.fuelsearcher.service.searcher.FuelSearcher;
 import by.aurorasoft.fuelsearcher.service.searchersparser.FuelSearchersParser;
+import by.aurorasoft.fuelsearcher.service.validator.SpecificationValidator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,6 +28,11 @@ public class FuelConfiguration {
     public List<FuelSearcher> fuelSearchers(final FuelSearchersParser parser,
                                             @Value("${fuel-searcher-config.path}") final String filePath) {
         return parser.parse(filePath);
+    }
+
+    @Bean
+    public List<SpecificationValidator> specificationValidators(final SpecificationValidatorsFactory factory) {
+        return factory.create();
     }
 
 }
--- src/main/java/by/aurorasoft/fuelsearcher/service/dictionary/SpecificationValidatorDictionary.java
+++ src/main/java/by/aurorasoft/fuelsearcher/service/dictionary/SpecificationValidatorDictionary.java
@@ -3,11 +3,13 @@ package by.aurorasoft.fuelsearcher.service.dictionary;
 import by.aurorasoft.fuelsearcher.service.validator.SpecificationValidator;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public final class SpecificationValidatorDictionary extends Dictionary<SpecificationValidator> {
 
-    public SpecificationValidatorDictionary(final SearchersParsingResult searchersParsingResult) {
-        super(searchersParsingResult.getSpecificationValidators());
+    public SpecificationValidatorDictionary(final List<SpecificationValidator> validators) {
+        super(validators);
     }
 
 }
