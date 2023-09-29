7f579adb0a518bf6a000f90db28eb24dc87f1057
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 12:16:35 +0300

    refactor RoadGroupGroupFilterFactoryTest

--- src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/interim/group/RoadGroupGroupFilterFactoryTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/interim/group/RoadGroupGroupFilterFactoryTest.java
@@ -1,24 +1,24 @@
-//package by.aurorasoft.fuelsearcher.model.filter.factory.interim.group;
-//
-//import by.aurorasoft.fuelsearcher.model.filter.interim.group.RoadGroupGroupFilter;
-//import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.RoadGroupExtractor;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertSame;
-//import static org.mockito.Mockito.mock;
-//
-//public final class RoadGroupGroupFilterFactoryTest {
-//    private final RoadGroupGroupFilterFactory factory = new RoadGroupGroupFilterFactory(null);
-//
-//    @Test
-//    public void filterShouldBeCreated() {
-//        final RoadGroupExtractor givenRoadGroupExtractor = mock(RoadGroupExtractor.class);
-//        final int givenFiltrationCellIndex = 1;
-//
-//        final RoadGroupGroupFilter actual = this.factory.create(givenRoadGroupExtractor, givenFiltrationCellIndex);
-//
-//        assertSame(givenRoadGroupExtractor, actual.getFiltrationValueExtractor());
-//        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
-//    }
-//}
+package by.aurorasoft.fuelsearcher.model.filter.factory.interim.group;
+
+import by.aurorasoft.fuelsearcher.model.filter.interim.group.RoadGroupGroupFilter;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.RoadGroupExtractor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+
+public final class RoadGroupGroupFilterFactoryTest {
+    private final RoadGroupGroupFilterFactory factory = new RoadGroupGroupFilterFactory(null);
+
+    @Test
+    public void filterShouldBeCreated() {
+        final RoadGroupExtractor givenRoadGroupExtractor = mock(RoadGroupExtractor.class);
+        final int givenFiltrationCellIndex = 1;
+
+        final RoadGroupGroupFilter actual = this.factory.create(givenRoadGroupExtractor, givenFiltrationCellIndex);
+
+        assertSame(givenRoadGroupExtractor, actual.getPropertyExtractor());
+        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
+    }
+}
