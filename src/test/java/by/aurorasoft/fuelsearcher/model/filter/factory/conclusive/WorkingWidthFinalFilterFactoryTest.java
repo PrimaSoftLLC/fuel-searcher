a60723daa3a5f644f6216bd2a9c9873edc2dc975
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 12:04:03 +0300

    refactor WorkingWidthFinalFilterFactoryTest

--- src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/conclusive/WorkingWidthFinalFilterFactoryTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/conclusive/WorkingWidthFinalFilterFactoryTest.java
@@ -1,23 +1,23 @@
-//package by.aurorasoft.fuelsearcher.model.filter.factory.conclusive;
-//
-//import by.aurorasoft.fuelsearcher.model.filter.conclusive.WorkingWidthFinalFilter;
-//import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.WorkingWidthExtractor;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertSame;
-//import static org.mockito.Mockito.mock;
-//
-//public final class WorkingWidthFinalFilterFactoryTest {
-//    private final WorkingWidthFinalFilterFactory factory = new WorkingWidthFinalFilterFactory(null);
-//
-//    @Test
-//    public void filterShouldBeCreated() {
-//        final WorkingWidthExtractor givenWorkingWidthExtractor = mock(WorkingWidthExtractor.class);
-//        final int givenFiltrationCellIndex = 6;
-//
-//        final WorkingWidthFinalFilter actual = this.factory.create(givenWorkingWidthExtractor, givenFiltrationCellIndex);
-//        assertSame(givenWorkingWidthExtractor, actual.getFiltrationValueExtractor());
-//        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
-//    }
-//}
+package by.aurorasoft.fuelsearcher.model.filter.factory.conclusive;
+
+import by.aurorasoft.fuelsearcher.model.filter.conclusive.WorkingWidthFinalFilter;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.WorkingWidthExtractor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+
+public final class WorkingWidthFinalFilterFactoryTest {
+    private final WorkingWidthFinalFilterFactory factory = new WorkingWidthFinalFilterFactory(null);
+
+    @Test
+    public void filterShouldBeCreated() {
+        final WorkingWidthExtractor givenWorkingWidthExtractor = mock(WorkingWidthExtractor.class);
+        final int givenFiltrationCellIndex = 6;
+
+        final WorkingWidthFinalFilter actual = this.factory.create(givenWorkingWidthExtractor, givenFiltrationCellIndex);
+        assertSame(givenWorkingWidthExtractor, actual.getPropertyExtractor());
+        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
+    }
+}
