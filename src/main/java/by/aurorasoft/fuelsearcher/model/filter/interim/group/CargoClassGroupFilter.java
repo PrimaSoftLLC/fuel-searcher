4e88c81ce8c2dd6dbc903a6faaa295a759ac8396
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 13:47:55 +0300

    refactor CargoClassGroupFilter and CargoClassGroupFilterTest

--- src/main/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/CargoClassGroupFilter.java
+++ src/main/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/CargoClassGroupFilter.java
@@ -3,7 +3,7 @@ package by.aurorasoft.fuelsearcher.model.filter.interim.group;
 import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.CargoClassExtractor;
 
 public final class CargoClassGroupFilter extends GroupFilter {
-    private static final String GROUP_VALUE_REGEX = "Грузы (I|II|III|IV) класса";
+    private static final String GROUP_VALUE_REGEX = "Грузы ((I)|(II)|(III)|(IV)) класса";
 
     public CargoClassGroupFilter(final CargoClassExtractor cargoClassExtractor, final int filtrationCellIndex) {
         super(cargoClassExtractor, filtrationCellIndex, GROUP_VALUE_REGEX);
--- src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/CargoClassGroupFilterTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/CargoClassGroupFilterTest.java
@@ -2,18 +2,47 @@ package by.aurorasoft.fuelsearcher.model.filter.interim.group;
 
 import org.junit.Test;
 
+import java.util.Set;
+
 import static java.lang.Integer.MIN_VALUE;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public final class CargoClassGroupFilterTest {
 
     @Test
-    public void groupValueRegexShouldBeFound() {
+    public void groupValuesShouldMatchRegex() {
         final CargoClassGroupFilter givenFilter = new CargoClassGroupFilter(null, MIN_VALUE);
+        final Set<String> givenGroupValues = Set.of(
+                "Грузы I класса",
+                "Грузы II класса",
+                "Грузы III класса",
+                "Грузы IV класса"
+        );
 
-        final String actual = givenFilter.getGroupValueRegex();
-        final String expected = "Грузы (I|II|III|IV) класса";
-        assertEquals(expected, actual);
+        final String groupValueRegex = givenFilter.getGroupValueRegex();
+
+        final boolean groupValuesMatchRegex = givenGroupValues
+                .stream()
+                .allMatch(value -> value.matches(groupValueRegex));
+        assertTrue(groupValuesMatchRegex);
+    }
+
+    @Test
+    public void groupValuesShouldNotMatchRegex() {
+        final CargoClassGroupFilter givenFilter = new CargoClassGroupFilter(null, MIN_VALUE);
+        final Set<String> givenGroupValues = Set.of(
+                "Грузы 1 класса",
+                "Грузы класса",
+                "Грузы g класса",
+                "Грузы V класса"
+        );
+
+        final String groupValueRegex = givenFilter.getGroupValueRegex();
+
+        final boolean groupValuesNotMatchRegex = givenGroupValues
+                .stream()
+                .noneMatch(value -> value.matches(groupValueRegex));
+        assertTrue(groupValuesNotMatchRegex);
     }
 
 }
