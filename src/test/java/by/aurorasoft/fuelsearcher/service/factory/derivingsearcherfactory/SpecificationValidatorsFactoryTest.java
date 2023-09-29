1786ff63d398da4bcf2f1346ed262e292a836945
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 16:02:17 +0300

    add SpecificationValidatorsFactoryTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/service/factory/derivingsearcherfactory/SpecificationValidatorsFactoryTest.java
@@ -0,0 +1,11 @@
+package by.aurorasoft.fuelsearcher.service.factory.derivingsearcherfactory;
+
+import org.junit.Test;
+
+public final class SpecificationValidatorsFactoryTest {
+
+    @Test
+    public void a() {
+        throw new RuntimeException();
+    }
+}
