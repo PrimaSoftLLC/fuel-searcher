51112d4f2991b03b671403a97bfffc6da098642c
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 14:32:26 +0300

    refactor SoilTypeGroupFilterTest

--- src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/SoilTypeGroupFilterTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/SoilTypeGroupFilterTest.java
@@ -2,17 +2,43 @@ package by.aurorasoft.fuelsearcher.model.filter.interim.group;
 
 import org.junit.Test;
 
+import java.util.Set;
+
 import static java.lang.Integer.MIN_VALUE;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public final class SoilTypeGroupFilterTest {
+    private final SoilTypeGroupFilter filter = new SoilTypeGroupFilter(null, MIN_VALUE);
 
     @Test
-    public void groupValueRegexShouldBeFound() {
-        final SoilTypeGroupFilter givenFilter = new SoilTypeGroupFilter(null, MIN_VALUE);
+    public void groupValuesShouldMatchRegex() {
+        final String givenGroupValueRegex = this.filter.getGroupValueRegex();
+        final Set<String> givenGroupValues = Set.of(
+                "Минеральные почвы",
+                "Торфяные почвы",
+                "Легкие почвы",
+                "Средние почвы",
+                "Тяжелые почвы"
+        );
 
-        final String actual = givenFilter.getGroupValueRegex();
-        final String expected = "((Минеральные)|(Торфяные)|(Легкие)|(Средние)|(Тяжелые)) почвы";
-        assertEquals(expected, actual);
+        final boolean groupValuesMatchRegex = givenGroupValues
+                .stream()
+                .allMatch(value -> value.matches(givenGroupValueRegex));
+        assertTrue(groupValuesMatchRegex);
+    }
+
+    @Test
+    public void groupValuesShouldNotMatchRegex() {
+        final String givenGroupValueRegex = this.filter.getGroupValueRegex();
+        final Set<String> givenGroupValues = Set.of(
+                "Минеальные почвы",
+                "какие-то почвы",
+                " почвы"
+        );
+
+        final boolean groupValuesNotMatchRegex = givenGroupValues
+                .stream()
+                .noneMatch(value -> value.matches(givenGroupValueRegex));
+        assertTrue(groupValuesNotMatchRegex);
     }
 }
