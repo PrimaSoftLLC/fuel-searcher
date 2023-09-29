a9859633d2289c837039c4e0106dfa0b8f7f08b0
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 12:00:08 +0300

    refactor SpreadRateFinalFilterFactoryTest

--- src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/conclusive/SpreadRateFinalFilterFactoryTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/conclusive/SpreadRateFinalFilterFactoryTest.java
@@ -1,23 +1,23 @@
-//package by.aurorasoft.fuelsearcher.model.filter.factory.conclusive;
-//
-//import by.aurorasoft.fuelsearcher.model.filter.conclusive.SpreadRateFinalFilter;
-//import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.SpreadRateExtractor;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertSame;
-//import static org.mockito.Mockito.mock;
-//
-//public final class SpreadRateFinalFilterFactoryTest {
-//    private final SpreadRateFinalFilterFactory factory = new SpreadRateFinalFilterFactory(null);
-//
-//    @Test
-//    public void filterShouldBeCreated() {
-//        final SpreadRateExtractor givenSpreadRateExtractor = mock(SpreadRateExtractor.class);
-//        final int givenFiltrationCellIndex = 2;
-//
-//        final SpreadRateFinalFilter actual = this.factory.create(givenSpreadRateExtractor, givenFiltrationCellIndex);
-//        assertSame(givenSpreadRateExtractor, actual.getFiltrationValueExtractor());
-//        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
-//    }
-//}
+package by.aurorasoft.fuelsearcher.model.filter.factory.conclusive;
+
+import by.aurorasoft.fuelsearcher.model.filter.conclusive.SpreadRateFinalFilter;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.SpreadRateExtractor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+
+public final class SpreadRateFinalFilterFactoryTest {
+    private final SpreadRateFinalFilterFactory factory = new SpreadRateFinalFilterFactory(null);
+
+    @Test
+    public void filterShouldBeCreated() {
+        final SpreadRateExtractor givenSpreadRateExtractor = mock(SpreadRateExtractor.class);
+        final int givenFiltrationCellIndex = 2;
+
+        final SpreadRateFinalFilter actual = this.factory.create(givenSpreadRateExtractor, givenFiltrationCellIndex);
+        assertSame(givenSpreadRateExtractor, actual.getPropertyExtractor());
+        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
+    }
+}
