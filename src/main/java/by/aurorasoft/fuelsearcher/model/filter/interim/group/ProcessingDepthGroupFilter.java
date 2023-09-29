e1422a3c711a5ffc98682a8a2e0a6406da6f5168
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 14:09:46 +0300

    refactor ProcessingDepthGroupFilter and ProcessingDepthGroupFilterTest

--- src/main/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/ProcessingDepthGroupFilter.java
+++ src/main/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/ProcessingDepthGroupFilter.java
@@ -3,7 +3,7 @@ package by.aurorasoft.fuelsearcher.model.filter.interim.group;
 import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.ProcessingDepthExtractor;
 
 public final class ProcessingDepthGroupFilter extends GroupFilter {
-    private static final String GROUP_VALUE_REGEX = "Глубина обработки \\d+...\\d+ см";
+    private static final String GROUP_VALUE_REGEX = "Глубина обработки \\d+\\.{3}\\d+ см";
 
     public ProcessingDepthGroupFilter(final ProcessingDepthExtractor processingDepthExtractor,
                                       final int filtrationCellIndex) {
