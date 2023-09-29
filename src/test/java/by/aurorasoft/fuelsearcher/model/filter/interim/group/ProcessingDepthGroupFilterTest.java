3c587accb874e87a671653e1747fb1eb4d220ce3
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 14:10:00 +0300

    refactor ProcessingDepthGroupFilterTest

--- src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/ProcessingDepthGroupFilterTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/ProcessingDepthGroupFilterTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import java.util.Set;
 
 import static java.lang.Integer.MIN_VALUE;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public final class ProcessingDepthGroupFilterTest {
