df381100637c53fae54f19d91ded6d5314335ef8
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 12:09:55 +0300

    refactor CargoClassGroupFilterFactoryTest

--- src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/interim/group/CargoClassGroupFilterFactoryTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/factory/interim/group/CargoClassGroupFilterFactoryTest.java
@@ -1,24 +1,24 @@
-//package by.aurorasoft.fuelsearcher.model.filter.factory.interim.group;
-//
-//import by.aurorasoft.fuelsearcher.model.filter.interim.group.CargoClassGroupFilter;
-//import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.CargoClassExtractor;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertSame;
-//import static org.mockito.Mockito.mock;
-//
-//public final class CargoClassGroupFilterFactoryTest {
-//    private final CargoClassGroupFilterFactory factory = new CargoClassGroupFilterFactory(null);
-//
-//    @Test
-//    public void filterShouldBeCreated() {
-//        final CargoClassExtractor givenCargoClassExtractor = mock(CargoClassExtractor.class);
-//        final int givenFiltrationCellIndex = 5;
-//
-//        final CargoClassGroupFilter actual = this.factory.create(givenCargoClassExtractor, givenFiltrationCellIndex);
-//
-//        assertSame(givenCargoClassExtractor, actual.getFiltrationValueExtractor());
-//        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
-//    }
-//}
+package by.aurorasoft.fuelsearcher.model.filter.factory.interim.group;
+
+import by.aurorasoft.fuelsearcher.model.filter.interim.group.CargoClassGroupFilter;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.CargoClassExtractor;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.mock;
+
+public final class CargoClassGroupFilterFactoryTest {
+    private final CargoClassGroupFilterFactory factory = new CargoClassGroupFilterFactory(null);
+
+    @Test
+    public void filterShouldBeCreated() {
+        final CargoClassExtractor givenCargoClassExtractor = mock(CargoClassExtractor.class);
+        final int givenFiltrationCellIndex = 5;
+
+        final CargoClassGroupFilter actual = this.factory.create(givenCargoClassExtractor, givenFiltrationCellIndex);
+
+        assertSame(givenCargoClassExtractor, actual.getPropertyExtractor());
+        assertEquals(givenFiltrationCellIndex, actual.getFiltrationCellIndex());
+    }
+}
