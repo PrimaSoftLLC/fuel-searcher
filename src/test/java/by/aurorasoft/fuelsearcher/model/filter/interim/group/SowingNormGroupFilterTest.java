1768ee77c25c4adf922e1ff7e1789056bdfaedc0
Vlad Zuev <vladzuev.00@mail.ru>
2023-09-29 14:36:45 +0300

    refactor SowingNormGroupFilterTest

--- src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/SowingNormGroupFilterTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/model/filter/interim/group/SowingNormGroupFilterTest.java
@@ -2,18 +2,43 @@ package by.aurorasoft.fuelsearcher.model.filter.interim.group;
 
 import org.junit.Test;
 
+import java.util.Set;
+
 import static java.lang.Integer.MIN_VALUE;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public final class SowingNormGroupFilterTest {
+    private final SowingNormGroupFilter filter = new SowingNormGroupFilter(null, MIN_VALUE);
 
     @Test
-    public void groupValueRegexShouldBeFound() {
-        final SowingNormGroupFilter givenFilter = new SowingNormGroupFilter(null, MIN_VALUE);
+    public void groupValuesShouldMatchRegex() {
+        final String givenGroupValueRegex = this.filter.getGroupValueRegex();
+        final Set<String> givenGroupValues = Set.of(
+                "Норма высева 3-5 кг/га",
+                "Норма высева семян 3-55 кг/га",
+                "Норма высева семян 3 кг/га",
+                "Норма высева 1 кг/га"
+        );
 
-        final String actual = givenFilter.getGroupValueRegex();
-        final String expected = "Норма высева (семян )?\\d+(-\\d+)? кг/га";
-        assertEquals(expected, actual);
+        final boolean groupValuesMatchRegex = givenGroupValues
+                .stream()
+                .allMatch(value -> value.matches(givenGroupValueRegex));
+        assertTrue(groupValuesMatchRegex);
     }
 
+    @Test
+    public void groupValuesShouldNotMatchRegex() {
+        final String givenGroupValueRegex = this.filter.getGroupValueRegex();
+        final Set<String> givenGroupValues = Set.of(
+                "Норма высева -1-5 кг/га",
+                "Норма высева семян 3-55 кгга",
+                "Нома высева семян 3 кг/га",
+                "Норма высева кг/га"
+        );
+
+        final boolean groupValuesNotMatchRegex = givenGroupValues
+                .stream()
+                .noneMatch(value -> value.matches(givenGroupValueRegex));
+        assertTrue(groupValuesNotMatchRegex);
+    }
 }
