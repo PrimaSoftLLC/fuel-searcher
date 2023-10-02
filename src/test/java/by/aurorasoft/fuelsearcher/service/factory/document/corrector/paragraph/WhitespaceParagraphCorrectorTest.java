40e03daabf9082ceecebb087704c02c778a9a0cc
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-02 13:40:16 +0300

    refactor WhitespaceParagraphCorrectorTest

--- src/test/java/by/aurorasoft/fuelsearcher/service/factory/document/corrector/paragraph/WhitespaceParagraphCorrectorTest.java
+++ src/test/java/by/aurorasoft/fuelsearcher/service/factory/document/corrector/paragraph/WhitespaceParagraphCorrectorTest.java
@@ -1,17 +1,48 @@
-//package by.aurorasoft.fuelsearcher.service.factory.document.corrector.paragraphcorrector;
-//
-//import by.aurorasoft.fuelsearcher.service.factory.documentfactory.corrector.paragraphcorrector.WhitespaceParagraphCorrector;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public final class WhitespaceParagraphCorrectorTest {
-//    private final WhitespaceParagraphCorrector corrector = new WhitespaceParagraphCorrector();
-//
-//    @Test
-//    public void replacementShouldBeCreated() {
-//        final String actual = this.corrector.createReplacement(null);
-//        final String expected = " ";
-//        assertEquals(expected, actual);
-//    }
-//}
+package by.aurorasoft.fuelsearcher.service.factory.document.corrector.paragraph;
+
+import org.junit.Test;
+
+import java.util.stream.Stream;
+
+import static by.aurorasoft.fuelsearcher.testutil.ReflectionUtil.findStaticFieldValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public final class WhitespaceParagraphCorrectorTest {
+    private static final String FIELD_NAME_REPLACED_REGEX = "REPLACED_REGEX";
+
+    private final WhitespaceParagraphCorrector corrector = new WhitespaceParagraphCorrector();
+
+    @Test
+    public void replacementShouldBeCreated() {
+        final String actual = this.corrector.createReplacement(null);
+        final String expected = " ";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void contentsShouldMatchReplacedRegex() {
+        final String givenReplacedRegex = findReplacedRegex();
+        final Stream<String> givenContents = Stream.of("   \u00A0    \u00A0   ", " ", "\u00A0");
+
+        final boolean actual = givenContents.allMatch(content -> content.matches(givenReplacedRegex));
+        assertTrue(actual);
+    }
+
+    @Test
+    public void contentShouldNotMatchReplacedRegex() {
+        final String givenReplacedRegex = findReplacedRegex();
+        final Stream<String> givenContents = Stream.of("    b  ", "");
+
+        final boolean actual = givenContents.noneMatch(content -> content.matches(givenReplacedRegex));
+        assertTrue(actual);
+    }
+
+    private static String findReplacedRegex() {
+        return findStaticFieldValue(
+                WhitespaceParagraphCorrector.class,
+                FIELD_NAME_REPLACED_REGEX,
+                String.class
+        );
+    }
+}
