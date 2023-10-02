add99cafad1c9a8c3de1625b071a1b764de02cb2
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-02 14:48:52 +0300

    add RoutingLengthHeaderMetadataFactoryTest

---
+++ src/test/java/by/aurorasoft/fuelsearcher/service/factory/fuelheadermetadata/RoutingLengthHeaderMetadataFactoryTest.java
@@ -0,0 +1,49 @@
+package by.aurorasoft.fuelsearcher.service.factory.fuelheadermetadata;
+
+import by.aurorasoft.fuelsearcher.model.header.FuelHeaderMetadata;
+import by.aurorasoft.fuelsearcher.model.header.RoutingLengthHeaderMetadata;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.RoutingLengthExtractor;
+import by.aurorasoft.fuelsearcher.model.specification.propertyextractor.SpecificationPropertyExtractor;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static by.aurorasoft.fuelsearcher.testutil.ReflectionUtil.findProperty;
+import static org.junit.Assert.assertSame;
+
+public final class RoutingLengthHeaderMetadataFactoryTest {
+    private static final String FIELD_NAME_FUEL_OFFSETS_BY_VALUES = "fuelOffsetsByValues";
+
+    private final RoutingLengthHeaderMetadataFactory factory = new RoutingLengthHeaderMetadataFactory(
+            null
+    );
+
+    @Test
+    public void headerMetadataShouldBeCreated() {
+        final RoutingLengthExtractor givenRoutingLengthExtractor = new RoutingLengthExtractor();
+        final Map<String, Integer> givenFuelOffsetsByHeaders = Map.of(
+                "first-header", 1,
+                "second-header", 2
+        );
+
+        final RoutingLengthHeaderMetadata actual = this.factory.create(
+                givenRoutingLengthExtractor,
+                givenFuelOffsetsByHeaders
+        );
+
+        final SpecificationPropertyExtractor actualPropertyExtractor = actual.getPropertyExtractor();
+        assertSame(givenRoutingLengthExtractor, actualPropertyExtractor);
+
+        final Map<String, Integer> actualFuelOffsetsByHeaders = findFuelOffsetsByValues(actual);
+        assertSame(givenFuelOffsetsByHeaders, actualFuelOffsetsByHeaders);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Map<String, Integer> findFuelOffsetsByValues(final FuelHeaderMetadata metadata) {
+        return findProperty(
+                metadata,
+                FIELD_NAME_FUEL_OFFSETS_BY_VALUES,
+                Map.class
+        );
+    }
+}
