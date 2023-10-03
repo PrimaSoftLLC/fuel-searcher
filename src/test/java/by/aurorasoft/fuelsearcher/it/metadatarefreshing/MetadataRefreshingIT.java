209a780d4a15a86cd10d9691537ea7aa3cd79c67
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-03 22:32:03 +0300

    refactor ThirdTableMetadataArgumentsProvider

--- src/test/java/by/aurorasoft/fuelsearcher/it/metadatarefreshing/MetadataRefreshingIT.java
+++ src/test/java/by/aurorasoft/fuelsearcher/it/metadatarefreshing/MetadataRefreshingIT.java
@@ -53,69 +53,6 @@ public final class MetadataRefreshingIT extends AbstractContextTest {
         @Override
         public Stream<? extends Arguments> provideArguments(final ExtensionContext extensionContext) {
             return Stream.of(
-                    //Table #1
-                    createMetadataJunitArguments(
-                            "ВСПАШКА ПЛАСТА МНОГОЛЕТНИХ ТРАВ",
-                            "длина гона",
-                            new String[]{"Менее 150", "150-200", "201-300", "301-400", "401-600", "601-1000", "Более 1000"}
-                    ),
-                    createMetadataJunitArguments(
-                            "ВСПАШКА ПЛАСТА МНОГОЛЕТНИХ ТРАВ",
-                            "удельное сопротивление",
-                            new String[]{"Удельное сопротивление 48...53 кПа", "Удельное сопротивление 54...59 кПа", "Удельное сопротивление 60...65 кПа"}
-                    ),
-                    createMetadataJunitArguments(
-                            "ВСПАШКА ПЛАСТА МНОГОЛЕТНИХ ТРАВ",
-                            "трактор",
-                            new String[]{"FENDT 1050", "Кировец К-744 Р4", "Кировец К-744 Р3", "Fendt 936 Vario", "Беларус 3525", "Fendt 936", "Беларус 3522", "Кировец K744P2", "John Deere 8520", "К 744Р3"}
-                    ),
-                    createMetadataJunitArguments(
-                            "ВСПАШКА ПЛАСТА МНОГОЛЕТНИХ ТРАВ",
-                            "механизм",
-                            new String[]{"Lemken Diamand 11", "ППО-8-40", "ППУ-13", "Kverneland RW 110", "Kverneland PG-100", "Vari Titan 10 7+3 L100", "ППШ-10-35", "Kuhn Challenger NSH-9", "ППО-9-45", "ППО-9-30/45"}
-                    ),
-                    createMetadataJunitArguments(
-                            "ВСПАШКА ПЛАСТА МНОГОЛЕТНИХ ТРАВ",
-                            "количество корпусов",
-                            new String[]{"9", "8", "13", "12", "10"}
-                    ),
-                    createMetadataJunitArguments(
-                            "ВСПАШКА ПЛАСТА МНОГОЛЕТНИХ ТРАВ",
-                            "глубина вспашки",
-                            new String[]{"18-20", "21-22", "23-25", "25-27"}
-                    ),
-
-                    //Table #2
-                    createMetadataJunitArguments(
-                            "ВСПАШКА СТЕРНИ",
-                            "длина гона",
-                            new String[]{"Менее 150", "150-200", "201-300", "301-400", "401-600", "601-1000", "Более 1000"}
-                    ),
-                    createMetadataJunitArguments(
-                            "ВСПАШКА СТЕРНИ",
-                            "удельное сопротивление",
-                            new String[]{"Удельное сопротивление 36...41 кПа", "Удельное сопротивление 42...47 кПа", "Удельное сопротивление 48...53 кПа", "Удельное сопротивление плуга 54...59 кПа", "Удельное сопротивление 60...65 кПа", "Удельное сопротивление 66...71 кПа"}
-                    ),
-                    createMetadataJunitArguments(
-                            "ВСПАШКА СТЕРНИ",
-                            "трактор",
-                            new String[]{"CASE II Steiger 550", "FENDT 100", "Кировец К-744 Р4", "Кировец К-744 Р3", "Massey Ferguson 8737S", "Fendt936 Vario", "Беларус 3525", "Fendt 936", "Беларус 3522", "Кировец K744 P2", "Case Magnum 340", "Jhon Deer 8530", "Беларус 3022", "Кировец К701", "John Deere 8520", "Fendt 927 Vario", "John Deere 8420", "Беларус 2522", "Кировец К-424", "Беларус 2022", "Беларус 1822", "Беларус 1523", "FENDT 1050"}
-                    ),
-                    createMetadataJunitArguments(
-                            "ВСПАШКА СТЕРНИ",
-                            "механизм",
-                            new String[]{"Lemken EuroTitan 10 8+3+1", "Lemken Diamand 11", "Lemken EuroTitan 10", "ППО-8-40К", "ПБН-6-50А", "ППУ-13", "Kverneland RW 110", "Lemkem Diamant 16", "Kverneland PG-100", "Lemken Diamant 11 7 L100", "Vari Titan 10 7+3 L100", "ППШ-10-35", "ППО-9-45", "ППО-8-40", "ППН-8-30/50", "ППО-9-45К", "ПОМ-6+1+1", "ПН-8-40", "ППН-7-30/50", "ПН-8-35У", "ППО-9-30/45", "KUHN Manager F10", "Amazone Cayron 200", "Lemken EuroDiamant 6", "ППН-7-30", "Lemken EurOpal-5", "ПЛН-5-35", "ПО-(4+1)-40", "ПКМ-5-40Р", "ПО 4-40", "ПОН-4-40", "ПЛН-4-35"}
-                    ),
-                    createMetadataJunitArguments(
-                            "ВСПАШКА СТЕРНИ",
-                            "количество корпусов",
-                            new String[]{"12", "9", "10", "8", "6", "13", "7", "5", "4"}
-                    ),
-                    createMetadataJunitArguments(
-                            "ВСПАШКА СТЕРНИ",
-                            "глубина вспашки",
-                            new String[]{"18-20", "21-22", "23-25", "25-27"}
-                    ),
 
                     //Table #3
                     createMetadataJunitArguments(
---
+++ src/test/java/by/aurorasoft/fuelsearcher/it/metadatarefreshing/model/argumentsprovider/table/ThirdTableMetadataArgumentsProvider.java
@@ -0,0 +1,45 @@
+package by.aurorasoft.fuelsearcher.it.metadatarefreshing.model.argumentsprovider.table;
+
+import by.aurorasoft.fuelsearcher.it.metadatarefreshing.model.PropertyMetadataArguments;
+
+import java.util.stream.Stream;
+
+public final class ThirdTableMetadataArgumentsProvider extends TableMetadataArgumentsProvider {
+    private static final String TABLE_NAME = "ВСПАШКА МЕЛИОРИРУЕМЫХ ЗЕМЕЛЬ";
+
+    public ThirdTableMetadataArgumentsProvider() {
+        super(TABLE_NAME);
+    }
+
+    @Override
+    protected Stream<PropertyMetadataArguments> createPropertyMetadataArguments(
+            final PropertyMetadataArgumentsFactory factory
+    ) {
+        return Stream.of(
+                factory.create(
+                        "длина гона",
+                        new String[]{"Менее 150", "150-200", "201-300", "301-400", "401-600", "601-1000", "Более 1000"}
+                ),
+                factory.create(
+                        "тип почвы",
+                        new String[]{"Минеральные почвы", "Торфяные почвы"}
+                ),
+                factory.create(
+                        "трактор",
+                        new String[]{"Беларус-3522", "Беларус-3022"}
+                ),
+                factory.create(
+                        "механизм",
+                        new String[]{"ПБН-6-50А"}
+                ),
+                factory.create(
+                        "количество корпусов",
+                        new String[]{"6"}
+                ),
+                factory.create(
+                        "глубина вспашки",
+                        new String[]{"21-22", "23-25", "25-27", "27-30", "30-35"}
+                )
+        );
+    }
+}
