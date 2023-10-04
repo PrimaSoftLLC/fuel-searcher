b7f8b5dcc708f9ea382968883dba39c49ef08f81
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-04 12:41:38 +0300

    add TwentyThirdTablePropertyMetadataViewsProvider

---
+++ src/test/java/com/aurorasoft/fuelsearcher/it/metadatarefreshing/model/argumentsprovider/table/TwentyThirdTablePropertyMetadataViewsProvider.java
@@ -0,0 +1,35 @@
+package com.aurorasoft.fuelsearcher.it.metadatarefreshing.model.argumentsprovider.table;
+
+import com.aurorasoft.fuelsearcher.it.metadatarefreshing.model.PropertyMetadataView;
+
+import java.util.stream.Stream;
+
+public final class TwentyThirdTablePropertyMetadataViewsProvider extends TablePropertyMetadataViewsProvider {
+    private static final String TABLE_NAME = "ПОДБОР ПРОВЯЛЕННЫХ ТРАВ ИЗ ВАЛКОВ С ИЗМЕЛЬЧЕНИЕМ И ПОДАЧЕЙ В ТРАНСПОРТНЫЕ СРЕДСТВА";
+
+    public TwentyThirdTablePropertyMetadataViewsProvider() {
+        super(TABLE_NAME);
+    }
+
+    @Override
+    protected Stream<PropertyMetadataView> createViews(final PropertyMetadataViewFactory factory) {
+        return Stream.of(
+                factory.create(
+                        "длина гона",
+                        new String[]{"Менее 150", "150-200", "201-300", "301-400", "401-600", "601-1000", "Более 1000"}
+                ),
+                factory.create(
+                        "механизм",
+                        new String[]{"BIG X 770", "BIG X 700", "NEW HOLLAND 600", "JOHN DEERE 8400", "JAGUAR 870", "JAGUAR 850", "КВК 800"}
+                ),
+                factory.create(
+                        "ширина захвата",
+                        new String[]{"3.8", "3", "4"}
+                ),
+                factory.create(
+                        "урожайность",
+                        new String[]{"5-7.5", "7.5-8.5", "8.5-9.5", "9.5-10.5", "10.5-11.5", "11.5-12.5", "12.5-13.5", "13.5-14.5", "14.5-15.5", "15.5-16.5", "16.5-17.5"}
+                )
+        );
+    }
+}
