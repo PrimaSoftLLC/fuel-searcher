71fee5322cf5117579fa0a4420ec80e922418c61
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-05 01:03:46 +0300

    refactor FuelTableTest

--- src/test/java/com/aurorasoft/fuelsearcher/model/FuelTableTest.java
+++ src/test/java/com/aurorasoft/fuelsearcher/model/FuelTableTest.java
@@ -2,7 +2,7 @@ package com.aurorasoft.fuelsearcher.model;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
 
 public final class FuelTableTest {
 
@@ -14,7 +14,7 @@ public final class FuelTableTest {
                 .build();
 
         final String actual = givenTable.findAlias();
-        assertEquals(givenTableName, actual);
+        assertSame(givenTableName, actual);
     }
 
 }
