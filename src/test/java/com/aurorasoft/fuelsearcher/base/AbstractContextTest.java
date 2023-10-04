2d09450ace7fe30df6c56305fe07d6d474ded40b
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-04 14:50:51 +0300

    refactor AbstractContextTest

--- src/test/java/com/aurorasoft/fuelsearcher/base/AbstractContextTest.java
+++ src/test/java/com/aurorasoft/fuelsearcher/base/AbstractContextTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +23,7 @@ import static java.util.TimeZone.setDefault;
 import static org.junit.Assert.assertEquals;
 import static org.testcontainers.utility.DockerImageName.parse;
 
+@DirtiesContext
 @Transactional
 @RunWith(SpringRunner.class)
 @SpringBootTest
