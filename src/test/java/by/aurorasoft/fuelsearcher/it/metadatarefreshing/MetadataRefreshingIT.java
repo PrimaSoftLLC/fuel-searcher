7f6b0fedfed0c8a4363659b84f83ef379f1e0042
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-04 11:37:55 +0300

    refactor MetadataRefreshingIT

--- src/test/java/by/aurorasoft/fuelsearcher/it/metadatarefreshing/MetadataRefreshingIT.java
+++ src/test/java/by/aurorasoft/fuelsearcher/it/metadatarefreshing/MetadataRefreshingIT.java
@@ -4,43 +4,39 @@ import by.aurorasoft.fuelsearcher.base.AbstractContextTest;
 import by.aurorasoft.fuelsearcher.it.metadatarefreshing.model.PropertyMetadataView;
 import by.aurorasoft.fuelsearcher.it.metadatarefreshing.model.argumentsprovider.DocumentPropertyMetadataViewsProvider;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.Test;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 
-import static by.aurorasoft.fuelsearcher.testutil.CollectionUtil.areEqualIgnoringOrder;
-import static org.junit.Assert.assertTrue;
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.context.support.TestPropertySourceUtils.addInlinedPropertiesToEnvironment;
 
 @ContextConfiguration(initializers = {MetadataRefreshingIT.MetadataRefreshingEnablePropertyOverrider.class})
 public final class MetadataRefreshingIT extends AbstractContextTest {
-    private static final String HQL_QUERY_TO_FIND_PROPERTY_ALLOWABLE_VALUES
-            = "SELECT e.allowableValues FROM PropertyMetadataEntity e "
-            + "WHERE e.propertyName = :propertyName AND e.tableMetadata.tableName = :tableName";
-    private static final String PARAMETER_NAME_PROPERTY_NAME = "propertyName";
-    private static final String PARAMETER_NAME_TABLE_NAME = "tableName";
+    private static final String HQL_QUERY_TO_FIND_ALL_PROPERTY_METADATA_VIEWS = "SELECT new "
+            + "by.aurorasoft.fuelsearcher.it.metadatarefreshing.model.PropertyMetadataView("
+            + "e.tableMetadata.tableName, e.propertyName, e.allowableValues) "
+            + "FROM PropertyMetadataEntity e";
 
-    @ParameterizedTest
-    @ArgumentsSource(DocumentPropertyMetadataViewsProvider.class)
-    public void propertyMetadataShouldBeRefreshed(final PropertyMetadataView arguments) {
-        final String givenTableName = arguments.tableName();
-        final String givenPropertyName = arguments.propertyName();
+    private final DocumentPropertyMetadataViewsProvider expectedViewsProvider
+            = new DocumentPropertyMetadataViewsProvider();
 
-        final String[] actualPropertyAllowableValues = this.findPropertyAllowableValues(
-                givenTableName,
-                givenPropertyName
-        );
-        final String[] expectedPropertyAllowableValues = arguments.expectedPropertyAllowableValues();
-        assertTrue(areEqualIgnoringOrder(expectedPropertyAllowableValues, actualPropertyAllowableValues));
+    @Test
+    public void propertyMetadataShouldBeRefreshed() {
+        final Set<PropertyMetadataView> actual = this.findPropertyMetadataViews();
+        final Set<PropertyMetadataView> expected = this.expectedViewsProvider.provide();
+        assertEquals(expected, actual);
     }
 
-    private String[] findPropertyAllowableValues(final String tableName, final String propertyName) {
-        return super.entityManager.createQuery(HQL_QUERY_TO_FIND_PROPERTY_ALLOWABLE_VALUES, String[].class)
-                .setParameter(PARAMETER_NAME_TABLE_NAME, tableName)
-                .setParameter(PARAMETER_NAME_PROPERTY_NAME, propertyName)
-                .getSingleResult();
+    private Set<PropertyMetadataView> findPropertyMetadataViews() {
+        return super.entityManager.createQuery(
+                HQL_QUERY_TO_FIND_ALL_PROPERTY_METADATA_VIEWS,
+                PropertyMetadataView.class
+        ).getResultStream().collect(toSet());
     }
 
     public static final class MetadataRefreshingEnablePropertyOverrider
