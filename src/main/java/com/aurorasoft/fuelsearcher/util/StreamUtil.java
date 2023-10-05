47db862284330ab555065dfb487dd72a109dc939
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-05 16:21:25 +0300

    refactor StreamUtil

--- src/main/java/com/aurorasoft/fuelsearcher/util/StreamUtil.java
+++ src/main/java/com/aurorasoft/fuelsearcher/util/StreamUtil.java
@@ -4,10 +4,10 @@ import lombok.experimental.UtilityClass;
 
 import java.util.Iterator;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import static java.util.function.Function.identity;
 import static java.util.stream.Stream.empty;
+import static java.util.stream.StreamSupport.stream;
 
 @UtilityClass
 public final class StreamUtil {
@@ -18,7 +18,7 @@ public final class StreamUtil {
 
     public static <T> Stream<T> asStream(final Iterator<T> iterator, final boolean parallel) {
         final Iterable<T> iterable = () -> iterator;
-        return StreamSupport.stream(iterable.spliterator(), parallel);
+        return stream(iterable.spliterator(), parallel);
     }
 
     @SafeVarargs
