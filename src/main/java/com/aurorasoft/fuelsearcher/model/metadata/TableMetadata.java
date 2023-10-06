d8f7c4efd2b32513774248a3ff23ee1e3500b514
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-06 12:31:57 +0300

    add TableMetadataDictionary

--- src/main/java/com/aurorasoft/fuelsearcher/model/metadata/TableMetadata.java
+++ src/main/java/com/aurorasoft/fuelsearcher/model/metadata/TableMetadata.java
@@ -1,5 +1,6 @@
 package com.aurorasoft.fuelsearcher.model.metadata;
 
+import com.aurorasoft.fuelsearcher.service.dictionary.Translatable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Value;
@@ -9,7 +10,12 @@ import java.util.List;
 @Value
 @AllArgsConstructor
 @Builder
-public class TableMetadata {
+public class TableMetadata implements Translatable {
     String tableName;
     List<PropertyMetadata> propertiesMetadata;
+
+    @Override
+    public String findAlias() {
+        return this.tableName;
+    }
 }
