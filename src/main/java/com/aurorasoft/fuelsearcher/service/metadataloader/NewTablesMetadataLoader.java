358afbc0db1c0e3a926bb84ab37e8744c2bc2195
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-06 14:29:19 +0300

    add NewTablesMetadataLoader

---
+++ src/main/java/com/aurorasoft/fuelsearcher/service/metadataloader/NewTablesMetadataLoader.java
@@ -0,0 +1,21 @@
+package com.aurorasoft.fuelsearcher.service.metadataloader;
+
+import com.aurorasoft.fuelsearcher.model.metadata.TableMetadata;
+import com.aurorasoft.fuelsearcher.service.factory.derivingsearcher.refreshedtablesmetadata.RefreshedTablesMetadataFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+@ConditionalOnProperty(prefix = "metadata-refreshing", name = "enable", havingValue = "true")
+public final class NewTablesMetadataLoader implements TablesMetadataLoader {
+    private final RefreshedTablesMetadataFactory refreshedTablesMetadataFactory;
+
+    @Override
+    public List<TableMetadata> load() {
+        return this.refreshedTablesMetadataFactory.create();
+    }
+}
