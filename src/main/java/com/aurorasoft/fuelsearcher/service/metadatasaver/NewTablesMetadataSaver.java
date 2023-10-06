f760004b56d25a8e0436318bc3a373cf114e41ea
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-06 14:46:38 +0300

    add NewTablesMetadataSaver

---
+++ src/main/java/com/aurorasoft/fuelsearcher/service/metadatasaver/NewTablesMetadataSaver.java
@@ -0,0 +1,70 @@
+package com.aurorasoft.fuelsearcher.service.metadatasaver;
+
+import com.aurorasoft.fuelsearcher.model.metadata.TableMetadata;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+@Component
+@ConditionalOnProperty(prefix = "metadata-refreshing", name = "enable", havingValue = "true")
+public final class NewTablesMetadataSaver {
+    private final String filePath;
+    private final List<TableMetadata> newTablesMetadata;
+
+    public NewTablesMetadataSaver(@Value("${metadata-refreshing.file-path}") final String filePath,
+                                  final List<TableMetadata> newTablesMetadata) {
+        this.filePath = filePath;
+        this.newTablesMetadata = newTablesMetadata;
+    }
+
+    public void save() {
+        try (final ObjectOutputStream outputStream = this.createObjectOutputStream()) {
+            this.newTablesMetadata.forEach(newTableMetadata -> save(newTableMetadata, outputStream));
+        } catch (final IOException cause) {
+            throw new NewTablesMetadataSavingException(cause);
+        }
+    }
+
+    private ObjectOutputStream createObjectOutputStream()
+            throws IOException {
+        final FileOutputStream fileOutputStream = new FileOutputStream(this.filePath);
+        final BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        return new ObjectOutputStream(bufferedOutputStream);
+    }
+
+    private static void save(final TableMetadata metadata, final ObjectOutputStream outputStream) {
+        try {
+            outputStream.writeObject(metadata);
+        } catch (final IOException cause) {
+            throw new NewTablesMetadataSavingException(cause);
+        }
+    }
+
+    private static final class NewTablesMetadataSavingException extends RuntimeException {
+
+        @SuppressWarnings("unused")
+        public NewTablesMetadataSavingException() {
+
+        }
+
+        @SuppressWarnings("unused")
+        public NewTablesMetadataSavingException(final String description) {
+            super(description);
+        }
+
+        public NewTablesMetadataSavingException(final Exception cause) {
+            super(cause);
+        }
+
+        @SuppressWarnings("unused")
+        public NewTablesMetadataSavingException(final String description, final Exception cause) {
+            super(description, cause);
+        }
+    }
+}
