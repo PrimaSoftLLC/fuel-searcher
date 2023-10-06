8af618e1d5ca7ab4e3e6bccf153a5cba761bea2d
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-06 12:14:30 +0300

    refactor TableMetadata

--- src/main/java/com/aurorasoft/fuelsearcher/model/metadata/TableMetadata.java
+++ src/main/java/com/aurorasoft/fuelsearcher/model/metadata/TableMetadata.java
@@ -1,9 +1,15 @@
 package com.aurorasoft.fuelsearcher.model.metadata;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Value;
 
+import java.util.List;
+
 @Value
+@AllArgsConstructor
+@Builder
 public class TableMetadata {
     String tableName;
-
+    List<PropertyMetadata> propertiesMetadata;
 }
