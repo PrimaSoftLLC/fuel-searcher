19d283541b38849988bb5d816b4c5890cb42a2c6
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-10 11:20:08 +0300

    add FileUtil and refactor FileUploadingService

--- src/main/java/com/aurorasoft/fuelsearcher/service/documentuploading/FileUploadingService.java
+++ src/main/java/com/aurorasoft/fuelsearcher/service/documentuploading/FileUploadingService.java
@@ -4,11 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static java.nio.file.Files.write;
+import static com.aurorasoft.fuelsearcher.util.FileUtil.rewriteFile;
 
 @Service
 public final class FileUploadingService {
@@ -24,36 +20,5 @@ public final class FileUploadingService {
     public void upload(final MultipartFile fuelDocument, final MultipartFile searcherConfigFile) {
         rewriteFile(fuelDocument, this.fuelDocumentPath);
         rewriteFile(searcherConfigFile, this.searcherConfigFilePath);
-    }
-
-    private static void rewriteFile(final MultipartFile file, final String filePath) {
-        try {
-            final Path path = Paths.get(filePath);
-            write(path, file.getBytes());
-        } catch (final IOException cause) {
-            throw new FileUploadingException(cause);
-        }
-    }
-
-    private static final class FileUploadingException extends RuntimeException {
-
-        @SuppressWarnings("unused")
-        public FileUploadingException() {
-
-        }
-
-        @SuppressWarnings("unused")
-        public FileUploadingException(final String description) {
-            super(description);
-        }
-
-        public FileUploadingException(final Exception cause) {
-            super(cause);
-        }
-
-        @SuppressWarnings("unused")
-        public FileUploadingException(final String description, final Exception cause) {
-            super(description, cause);
-        }
     }
 }
---
+++ src/main/java/com/aurorasoft/fuelsearcher/util/FileUtil.java
@@ -0,0 +1,43 @@
+package com.aurorasoft.fuelsearcher.util;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.Files.write;
+
+public final class FileUtil {
+
+    public static void rewriteFile(final MultipartFile file, final String filePath) {
+        try {
+            final Path path = Paths.get(filePath);
+            write(path, file.getBytes());
+        } catch (final IOException cause) {
+            throw new FileRewritingException(cause);
+        }
+    }
+
+    private static final class FileRewritingException extends RuntimeException {
+
+        @SuppressWarnings("unused")
+        public FileRewritingException() {
+
+        }
+
+        @SuppressWarnings("unused")
+        public FileRewritingException(final String description) {
+            super(description);
+        }
+
+        public FileRewritingException(final Exception cause) {
+            super(cause);
+        }
+
+        @SuppressWarnings("unused")
+        public FileRewritingException(final String description, final Exception cause) {
+            super(description, cause);
+        }
+    }
+}
