b75e4e15c1b10f48d4ff4893da971a93ec8e3d2a
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-10 17:50:41 +0300

    add NewTablesMetadataLoaderTest

---
+++ src/test/java/com/aurorasoft/fuelsearcher/service/metadataloader/NewTablesMetadataLoaderTest.java
@@ -0,0 +1,10 @@
+package com.aurorasoft.fuelsearcher.service.metadataloader;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public final class NewTablesMetadataLoaderTest {
+    private
+
+}
