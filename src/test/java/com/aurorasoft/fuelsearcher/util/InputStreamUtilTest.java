ac76de5f9653e48851a6a3a16b7005f76d1f1ab1
Vlad Zuev <vladzuev.00@mail.ru>
2023-10-11 18:31:23 +0300

    refactor InputStreamUtil::closeStream

--- src/test/java/com/aurorasoft/fuelsearcher/util/InputStreamUtilTest.java
+++ src/test/java/com/aurorasoft/fuelsearcher/util/InputStreamUtilTest.java
@@ -2,10 +2,14 @@ package com.aurorasoft.fuelsearcher.util;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 
+import static com.aurorasoft.fuelsearcher.util.InputStreamUtil.closeStream;
 import static com.aurorasoft.fuelsearcher.util.InputStreamUtil.createObjectInputStream;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
 
 public final class InputStreamUtilTest {
 
@@ -22,6 +26,26 @@ public final class InputStreamUtilTest {
         final String givenFilePath = "./src/test/resources/not-exist.txt";
 
         createObjectInputStream(givenFilePath);
+    }
+
+    @Test
+    public void streamShouldBeClosed()
+            throws Exception {
+        final InputStream givenInputStream = mock(InputStream.class);
+
+        closeStream(givenInputStream);
+
+        verify(givenInputStream, times(1)).close();
+    }
+
+    @Test(expected = Exception.class)
+    public void streamShouldNotBeClosed()
+            throws Exception {
+        final InputStream givenInputStream = mock(InputStream.class);
+
+        doThrow(IOException.class).when(givenInputStream).close();
+
+        closeStream(givenInputStream);
     }
 
 }
